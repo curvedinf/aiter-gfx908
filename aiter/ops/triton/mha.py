6bd4a6a240353d5cb9b67dce0844861d1c3f9fbd
Tianxing Wu <chi0chu319@gmail.com>
2025-08-27 12:19:19 +0000

    update

--- aiter/ops/triton/mha.py
+++ aiter/ops/triton/mha.py
@@ -378,40 +378,48 @@ def _process_tile(
     s_dmask_ptr: torch.Tensor,
     dropout_mask_ptr: torch.Tensor,
     softmax_lse_ptr: torch.Tensor,
-    stride_qz_in,
-    stride_qh_in,
-    stride_qm_in,
-    stride_qk_in,
-    stride_kz_in,
-    stride_kh_in,
-    stride_kn_in,
-    stride_kk_in,
-    stride_vz_in,
-    stride_vh_in,
-    stride_vn_in,
-    stride_vk_in,
-    stride_descale_q_z_in,
-    stride_descale_k_z_in,
-    stride_descale_v_z_in,
-    stride_oz_in,
-    stride_oh_in,
-    stride_om_in,
-    stride_on_in,
-    stride_alibi_z_in,
-    stride_alibi_h_in,
-    stride_sd_z_in,
-    stride_sd_h_in,
-    stride_sd_m_in,
-    stride_sd_n_in,
-    stride_lse_z_in,
-    stride_lse_h_in,
-    stride_lse_m_in,
+    stride_qz,
+    stride_qh,
+    stride_qm,
+    stride_qk,
+    stride_kz,
+    stride_kh,
+    stride_kn,
+    stride_kk,
+    stride_vz,
+    stride_vh,
+    stride_vn,
+    stride_vk,
+    stride_descale_q_z,
+    stride_descale_k_z,
+    stride_descale_v_z,
+    stride_oz,
+    stride_oh,
+    stride_om,
+    stride_on,
+    stride_alibi_z,
+    stride_alibi_h,
+    stride_sd_z,
+    stride_sd_h,
+    stride_sd_m,
+    stride_sd_n,
+    stride_lse_z,
+    stride_lse_h,
+    stride_lse_m,
     sm_scale: tl.constexpr,
     cu_seqlens_q,
     cu_seqlens_k,
     dropout_p,
     philox_seed,
-    philox_offset_base_in,
+    philox_offset_base,
+    offs_m: tl.constexpr,
+    offs_n: tl.constexpr,
+    offs_d: tl.constexpr,
+    _continue_condition,
+    cu_seqlens_q_start,
+    cu_seqlens_k_start,
+    seqlen_q,
+    seqlen_k,
     SEQLEN_Q: tl.constexpr,
     SEQLEN_K: tl.constexpr,
     IS_CAUSAL: tl.constexpr,
@@ -426,12 +434,9 @@ def _process_tile(
     IS_FP8: tl.constexpr,
     FP8_MAX: tl.constexpr,
     VARLEN: tl.constexpr,
-    USE_INT64_STRIDES: tl.constexpr,
 ):
     # offsets
-    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
-    offs_n = tl.arange(0, BLOCK_N)
-    offs_d = tl.arange(0, BLOCK_DMODEL_POW2)
+
 
     # NOTE:
     # Workaround for int64 strides, In the absence of strides being int64, parts of the offset
@@ -442,121 +447,7 @@ def _process_tile(
     # If we define new strides as stride_x: tl.int64 = stride_x_in, segfault remains
     # The permanent solution is to enable upcasting of tl.constexpr
     # In the meantime, the following workaround provides correctness and does not drop perf
-    if USE_INT64_STRIDES:
-        stride_qz = tl.cast(stride_qz_in, tl.int64)
-        stride_qh = tl.cast(stride_qh_in, tl.int64)
-        stride_qm = tl.cast(stride_qm_in, tl.int64)
-        stride_qk = tl.cast(stride_qk_in, tl.int64)
-        stride_kz = tl.cast(stride_kz_in, tl.int64)
-        stride_kh = tl.cast(stride_kh_in, tl.int64)
-        stride_kn = tl.cast(stride_kn_in, tl.int64)
-        stride_kk = tl.cast(stride_kk_in, tl.int64)
-        stride_vz = tl.cast(stride_vz_in, tl.int64)
-        stride_vh = tl.cast(stride_vh_in, tl.int64)
-        stride_vn = tl.cast(stride_vn_in, tl.int64)
-        stride_vk = tl.cast(stride_vk_in, tl.int64)
-        if IS_FP8:
-            stride_descale_q_z = tl.cast(stride_descale_q_z_in, tl.int64)
-            stride_descale_k_z = tl.cast(stride_descale_k_z_in, tl.int64)
-            stride_descale_v_z = tl.cast(stride_descale_v_z_in, tl.int64)
-        stride_oz = tl.cast(stride_oz_in, tl.int64)
-        stride_oh = tl.cast(stride_oh_in, tl.int64)
-        stride_om = tl.cast(stride_om_in, tl.int64)
-        stride_on = tl.cast(stride_on_in, tl.int64)
-        stride_alibi_z = tl.cast(stride_alibi_z_in, tl.int64)
-        stride_alibi_h = tl.cast(stride_alibi_h_in, tl.int64)
-
-        # NOTE: philox offset is need in dropout pointer calculations
-        philox_offset_base = tl.cast(philox_offset_base_in, tl.int64)
-        stride_sd_z = tl.cast(stride_sd_z_in, tl.int64)
-        stride_sd_h = tl.cast(stride_sd_h_in, tl.int64)
-        stride_sd_m = tl.cast(stride_sd_m_in, tl.int64)
-        stride_sd_n = tl.cast(stride_sd_n_in, tl.int64)
-        stride_lse_z = tl.cast(stride_lse_z_in, tl.int64)
-        stride_lse_h = tl.cast(stride_lse_h_in, tl.int64)
-        stride_lse_m = tl.cast(stride_lse_m_in, tl.int64)
-    else:
-        stride_qz = stride_qz_in
-        stride_qm = stride_qm_in
-        stride_qk = stride_qk_in
-        stride_qh = stride_qh_in
-        stride_kz = stride_kz_in
-        stride_kh = stride_kh_in
-        stride_kn = stride_kn_in
-        stride_kk = stride_kk_in
-        stride_vz = stride_vz_in
-        stride_vh = stride_vh_in
-        stride_vn = stride_vn_in
-        stride_vk = stride_vk_in
-        stride_descale_q_z = stride_descale_q_z_in
-        stride_descale_k_z = stride_descale_k_z_in
-        stride_descale_v_z = stride_descale_v_z_in
-        stride_oz = stride_oz_in
-        stride_oh = stride_oh_in
-        stride_om = stride_om_in
-        stride_on = stride_on_in
-        stride_alibi_z = stride_alibi_z_in
-        stride_alibi_h = stride_alibi_h_in
-        philox_offset_base = philox_offset_base_in
-        stride_sd_z = stride_sd_z_in
-        stride_sd_h = stride_sd_h_in
-        stride_sd_m = stride_sd_m_in
-        stride_sd_n = stride_sd_n_in
-        stride_lse_z = stride_lse_z_in
-        stride_lse_h = stride_lse_h_in
-        stride_lse_m = stride_lse_m_in
-
-    tl.assume(stride_qz >= 0)
-    tl.assume(stride_qh >= 0)
-    tl.assume(stride_qm >= 0)
-    tl.assume(stride_qk >= 0)
-    tl.assume(stride_kz >= 0)
-    tl.assume(stride_kh >= 0)
-    tl.assume(stride_kn >= 0)
-    tl.assume(stride_kk >= 0)
-    tl.assume(stride_vz >= 0)
-    tl.assume(stride_vh >= 0)
-    tl.assume(stride_vk >= 0)
-    tl.assume(stride_vn >= 0)
-    if IS_FP8:
-        tl.assume(stride_descale_q_z >= 0)
-        tl.assume(stride_descale_k_z >= 0)
-        tl.assume(stride_descale_v_z >= 0)
-    tl.assume(stride_oz >= 0)
-    tl.assume(stride_oh >= 0)
-    tl.assume(stride_om >= 0)
-    tl.assume(stride_on >= 0)
-    tl.assume(stride_alibi_z >= 0)
-    tl.assume(stride_alibi_h >= 0)
-    tl.assume(philox_offset_base >= 0)
-    tl.assume(stride_sd_z >= 0)
-    tl.assume(stride_sd_h >= 0)
-    tl.assume(stride_sd_m >= 0)
-    tl.assume(stride_sd_n >= 0)
-    tl.assume(stride_lse_z >= 0)
-    tl.assume(stride_lse_h >= 0)
-    tl.assume(stride_lse_m >= 0)
-
-    continue_condition = True
-
-    if VARLEN:
-        cu_seqlens_q_start = tl.load(cu_seqlens_q + off_z)
-        cu_seqlens_q_end = tl.load(cu_seqlens_q + off_z + 1)
-
-        seqlen_q = cu_seqlens_q_end - cu_seqlens_q_start
-        # We have a one-size-fits-all grid in id(0). Some seqlens might be too
-        # small for all start_m so for those we return early.
-        if start_m * BLOCK_M > seqlen_q:
-            continue_condition = False
-        cu_seqlens_k_start = tl.load(cu_seqlens_k + off_z)
-        cu_seqlens_k_end = tl.load(cu_seqlens_k + off_z + 1)
-        seqlen_k = cu_seqlens_k_end - cu_seqlens_k_start
-    else:
-        cu_seqlens_q_start = 0
-        cu_seqlens_k_start = 0
-        seqlen_q = SEQLEN_Q
-        seqlen_k = SEQLEN_K
-
+    continue_condition = _continue_condition
     if continue_condition:
 
 
@@ -1424,10 +1315,135 @@ def _attn_fwd_persistent_vanilla(
     workgroup_id = tl.program_id(
         0
     )  # persistent workgroup id ranging: 0,1,2,...., NUM_WGS - 1
-    for tile_id in tl.range(workgroup_id, num_tiles, step=NUM_WGS, flatten=True):
+    if USE_INT64_STRIDES:
+        stride_qz = tl.cast(stride_qz_in, tl.int64)
+        stride_qh = tl.cast(stride_qh_in, tl.int64)
+        stride_qm = tl.cast(stride_qm_in, tl.int64)
+        stride_qk = tl.cast(stride_qk_in, tl.int64)
+        stride_kz = tl.cast(stride_kz_in, tl.int64)
+        stride_kh = tl.cast(stride_kh_in, tl.int64)
+        stride_kn = tl.cast(stride_kn_in, tl.int64)
+        stride_kk = tl.cast(stride_kk_in, tl.int64)
+        stride_vz = tl.cast(stride_vz_in, tl.int64)
+        stride_vh = tl.cast(stride_vh_in, tl.int64)
+        stride_vn = tl.cast(stride_vn_in, tl.int64)
+        stride_vk = tl.cast(stride_vk_in, tl.int64)
+        if IS_FP8:
+            stride_descale_q_z = tl.cast(stride_descale_q_z_in, tl.int64)
+            stride_descale_k_z = tl.cast(stride_descale_k_z_in, tl.int64)
+            stride_descale_v_z = tl.cast(stride_descale_v_z_in, tl.int64)
+        else:
+            stride_descale_q_z = stride_descale_q_z_in
+            stride_descale_k_z = stride_descale_k_z_in
+            stride_descale_v_z = stride_descale_v_z_in
+        stride_oz = tl.cast(stride_oz_in, tl.int64)
+        stride_oh = tl.cast(stride_oh_in, tl.int64)
+        stride_om = tl.cast(stride_om_in, tl.int64)
+        stride_on = tl.cast(stride_on_in, tl.int64)
+        stride_alibi_z = tl.cast(stride_alibi_z_in, tl.int64)
+        stride_alibi_h = tl.cast(stride_alibi_h_in, tl.int64)
+
+        # NOTE: philox offset is need in dropout pointer calculations
+        philox_offset_base = tl.cast(philox_offset_base_in, tl.int64)
+        stride_sd_z = tl.cast(stride_sd_z_in, tl.int64)
+        stride_sd_h = tl.cast(stride_sd_h_in, tl.int64)
+        stride_sd_m = tl.cast(stride_sd_m_in, tl.int64)
+        stride_sd_n = tl.cast(stride_sd_n_in, tl.int64)
+        stride_lse_z = tl.cast(stride_lse_z_in, tl.int64)
+        stride_lse_h = tl.cast(stride_lse_h_in, tl.int64)
+        stride_lse_m = tl.cast(stride_lse_m_in, tl.int64)
+    else:
+        stride_qz = stride_qz_in
+        stride_qm = stride_qm_in
+        stride_qk = stride_qk_in
+        stride_qh = stride_qh_in
+        stride_kz = stride_kz_in
+        stride_kh = stride_kh_in
+        stride_kn = stride_kn_in
+        stride_kk = stride_kk_in
+        stride_vz = stride_vz_in
+        stride_vh = stride_vh_in
+        stride_vn = stride_vn_in
+        stride_vk = stride_vk_in
+        stride_descale_q_z = stride_descale_q_z_in
+        stride_descale_k_z = stride_descale_k_z_in
+        stride_descale_v_z = stride_descale_v_z_in
+        stride_oz = stride_oz_in
+        stride_oh = stride_oh_in
+        stride_om = stride_om_in
+        stride_on = stride_on_in
+        stride_alibi_z = stride_alibi_z_in
+        stride_alibi_h = stride_alibi_h_in
+        philox_offset_base = philox_offset_base_in
+        stride_sd_z = stride_sd_z_in
+        stride_sd_h = stride_sd_h_in
+        stride_sd_m = stride_sd_m_in
+        stride_sd_n = stride_sd_n_in
+        stride_lse_z = stride_lse_z_in
+        stride_lse_h = stride_lse_h_in
+        stride_lse_m = stride_lse_m_in
+
+    tl.assume(stride_qz >= 0)
+    tl.assume(stride_qh >= 0)
+    tl.assume(stride_qm >= 0)
+    tl.assume(stride_qk >= 0)
+    tl.assume(stride_kz >= 0)
+    tl.assume(stride_kh >= 0)
+    tl.assume(stride_kn >= 0)
+    tl.assume(stride_kk >= 0)
+    tl.assume(stride_vz >= 0)
+    tl.assume(stride_vh >= 0)
+    tl.assume(stride_vk >= 0)
+    tl.assume(stride_vn >= 0)
+    if IS_FP8:
+        tl.assume(stride_descale_q_z >= 0)
+        tl.assume(stride_descale_k_z >= 0)
+        tl.assume(stride_descale_v_z >= 0)
+    tl.assume(stride_oz >= 0)
+    tl.assume(stride_oh >= 0)
+    tl.assume(stride_om >= 0)
+    tl.assume(stride_on >= 0)
+    tl.assume(stride_alibi_z >= 0)
+    tl.assume(stride_alibi_h >= 0)
+    tl.assume(philox_offset_base >= 0)
+    tl.assume(stride_sd_z >= 0)
+    tl.assume(stride_sd_h >= 0)
+    tl.assume(stride_sd_m >= 0)
+    tl.assume(stride_sd_n >= 0)
+    tl.assume(stride_lse_z >= 0)
+    tl.assume(stride_lse_h >= 0)
+    tl.assume(stride_lse_m >= 0)
+
+    offs_n = tl.arange(0, BLOCK_N)
+    offs_d = tl.arange(0, BLOCK_DMODEL_POW2)
+
+    for tile_id in tl.range(workgroup_id, num_tiles, step=NUM_WGS, num_stages=2):
         off_q_head = tile_id % NUM_Q_HEADS
         start_m = tile_id // NUM_Q_HEADS % NUM_BLOCKS
         off_z = tile_id // (NUM_Q_HEADS * NUM_BLOCKS)
+        off_z_old = (tile_id - 1) // (NUM_Q_HEADS * NUM_BLOCKS)
+        offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+
+        continue_condition = True
+
+        if VARLEN and (not (off_z == off_z_old)):
+            cu_seqlens_q_start = tl.load(cu_seqlens_q + off_z)
+            cu_seqlens_q_end = tl.load(cu_seqlens_q + off_z + 1)
+
+            seqlen_q = cu_seqlens_q_end - cu_seqlens_q_start
+            # We have a one-size-fits-all grid in id(0). Some seqlens might be too
+            # small for all start_m so for those we return early.
+            if start_m * BLOCK_M > seqlen_q:
+                continue_condition = False
+            cu_seqlens_k_start = tl.load(cu_seqlens_k + off_z)
+            cu_seqlens_k_end = tl.load(cu_seqlens_k + off_z + 1)
+            seqlen_k = cu_seqlens_k_end - cu_seqlens_k_start
+        else:
+            cu_seqlens_q_start = 0
+            cu_seqlens_k_start = 0
+            seqlen_q = SEQLEN_Q
+            seqlen_k = SEQLEN_K
+
         _process_tile(
             start_m,
             off_z,
@@ -1443,40 +1459,48 @@ def _attn_fwd_persistent_vanilla(
             s_dmask_ptr,
             dropout_mask_ptr,
             softmax_lse_ptr,
-            stride_qz_in,
-            stride_qh_in,
-            stride_qm_in,
-            stride_qk_in,
-            stride_kz_in,
-            stride_kh_in,
-            stride_kn_in,
-            stride_kk_in,
-            stride_vz_in,
-            stride_vh_in,
-            stride_vn_in,
-            stride_vk_in,
-            stride_descale_q_z_in,
-            stride_descale_k_z_in,
-            stride_descale_v_z_in,
-            stride_oz_in,
-            stride_oh_in,
-            stride_om_in,
-            stride_on_in,
-            stride_alibi_z_in,
-            stride_alibi_h_in,
-            stride_sd_z_in,
-            stride_sd_h_in,
-            stride_sd_m_in,
-            stride_sd_n_in,
-            stride_lse_z_in,
-            stride_lse_h_in,
-            stride_lse_m_in,
+            stride_qz,
+            stride_qh,
+            stride_qm,
+            stride_qk,
+            stride_kz,
+            stride_kh,
+            stride_kn,
+            stride_kk,
+            stride_vz,
+            stride_vh,
+            stride_vn,
+            stride_vk,
+            stride_descale_q_z,
+            stride_descale_k_z,
+            stride_descale_v_z,
+            stride_oz,
+            stride_oh,
+            stride_om,
+            stride_on,
+            stride_alibi_z,
+            stride_alibi_h,
+            stride_sd_z,
+            stride_sd_h,
+            stride_sd_m,
+            stride_sd_n,
+            stride_lse_z,
+            stride_lse_h,
+            stride_lse_m,
             sm_scale,
             cu_seqlens_q,
             cu_seqlens_k,
             dropout_p,
             philox_seed,
-            philox_offset_base_in,
+            philox_offset_base,
+            offs_m,
+            offs_n,
+            offs_d,
+            continue_condition,
+            cu_seqlens_q_start,
+            cu_seqlens_k_start,
+            seqlen_q,
+            seqlen_k,
             SEQLEN_Q,
             SEQLEN_K,
             IS_CAUSAL,
@@ -1490,8 +1514,7 @@ def _attn_fwd_persistent_vanilla(
             ENABLE_DROPOUT,
             IS_FP8,
             FP8_MAX,
-            VARLEN,
-            USE_INT64_STRIDES)
+            VARLEN)
 
     
 @triton.jit

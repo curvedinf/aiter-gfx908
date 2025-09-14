01db8e92b31a84f4b0f11ccc30350af37345594b
ZhangLirong-amd <Lirong.Zhang@amd.com>
2025-09-14 17:11:10 +0800

    remove is_compiling

--- aiter/jit/core.py
+++ aiter/jit/core.py
@@ -877,9 +877,7 @@ def compile_ops(
 
             return op(*args, **kwargs)
 
-        import torch
-
-        if func.__name__ in NONE_WRAPPED_OP or not torch.compiler.is_compiling():
+        if func.__name__ in NONE_WRAPPED_OP:
             return wrapper
 
         def wrapper_register(func):

276c5f9f4ad41fa7fd5d7da0e96805f399fdbbbb
k50112113 <Shao-Chun.Lee@amd.com>
2025-09-16 21:16:00 -0500

    Revert "[Triton] Shaoclee/085 triton fusion (#1009)" (#1013)
    This reverts commit 3164d365e666c7e8c168973705049c3877cc550c.

--- aiter/ops/triton/fused_qk_concat.py
+++ aiter/ops/triton/fused_qk_concat.py
@@ -744,8 +744,7 @@ def fused_qk_rope_cat_and_cache_mla(
         BLOCK_D_pe=d_pe,
         BLOCK_D_HALF_pe=d_pe // 2,
         OUTPUT_Q_NOPE_ZEROS=output_q_nope_zeros,
-        HAS_K_SCALE=(k_scale is not None),
-        num_warps=1,
+        HAS_K_SCALE=(k_scale is not None)
     )
 
     if output_q_nope_zeros:

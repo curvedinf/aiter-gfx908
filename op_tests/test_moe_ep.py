53e02c67de73d7d3bd618afd5ce4365ef0ec998b
chenjun <junchen2@amd.com>
2025-09-23 03:44:33 +0000

    re add asm moe smf 32x384

--- op_tests/test_moe_ep.py
+++ op_tests/test_moe_ep.py
@@ -312,7 +312,7 @@ def test_fmoe_ep(
                 and (w1b.dtype == dtypes.fp8 and inter_dim * 2 == w1b.shape[1])
             )
             or (
-                (inter_dim % 320 == 0)
+                (inter_dim % 256 == 0 or inter_dim % 320 == 0 or inter_dim % 384 == 0)
                 and (w1b.dtype == dtypes.i8 and inter_dim * 2 == w1b.shape[1])
             )
             or (
@@ -593,7 +593,7 @@ for test in l_test:
                                 topk,
                                 quant="int8smoothquant",
                                 use_g1u1=True,
-                                shared_E=2,
+                                shared_E=0,
                                 ep=ep,
                             )
     elif test == "g1u1_fp8smoothquant":

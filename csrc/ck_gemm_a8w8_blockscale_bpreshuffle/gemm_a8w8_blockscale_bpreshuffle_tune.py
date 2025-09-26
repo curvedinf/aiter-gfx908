a9da7a0911ea508267317f69006f2791a518b2b1
bingzhou <BingYuan.Zhou@amd.com>
2025-09-26 03:37:07 +0000

    fix the issue of writing all tuning results to excel and standardize the tuning interface

--- csrc/ck_gemm_a8w8_blockscale_bpreshuffle/gemm_a8w8_blockscale_bpreshuffle_tune.py
+++ csrc/ck_gemm_a8w8_blockscale_bpreshuffle/gemm_a8w8_blockscale_bpreshuffle_tune.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 import argparse
 import os
-
 import pandas as pd
 import torch
 import torch.nn.functional as F
@@ -13,6 +12,7 @@ import aiter
 from aiter import dtypes
 from aiter.ops.shuffle import shuffle_weight
 from aiter.test_common import perftest
+from aiter.utility.base_tuner import GemmCommonTuner
 from aiter.utility.mp_tuner import mp_tuner
 
 block_shape = (128, 128)
@@ -37,8 +37,6 @@ def run_torch(x, weight, x_scale, w_scale, bias=None, dtype=dtypes.bf16):
     n = weight.shape[0]
     scale_n = (n + block_shape_n - 1) // block_shape_n
     scale_k = (k + block_shape_k - 1) // block_shape_k
-    # x_scale = rearrange(x_scale.view(-1, 1).repeat(1, block_shape_n*block_shape_k).view(m, scale_k, 1, block_shape_k),
-    #                           'num_blk_n num_blk_k blk_n blk_k ->(num_blk_n blk_n) (num_blk_k blk_k)')
     x = x.to(x_scale.dtype).view(
         m, k // block_shape[1], block_shape[1]
     ) * x_scale.unsqueeze(-1)
@@ -54,30 +52,9 @@ def run_torch(x, weight, x_scale, w_scale, bias=None, dtype=dtypes.bf16):
     weight = weight.to(w_scale.dtype) * w_scale
 
     out = F.linear(x.to(dtypes.fp32), weight.to(dtypes.fp32))
-    # scale = torch.matmul(x_scale, w_scale)
-    # out = torch.mul(x, scale)
     if bias is not None:
         out = out.to(bias) + bias
     return out.to(dtype)
-
-
-def get_untuned_gemm_list(untuned_gemm_file):
-    assert os.path.exists(
-        untuned_gemm_file
-    ), f"Not exist a8w8_untuned_gemm.csv file: {untuned_gemm_file}"
-    untunedf = pd.read_csv(untuned_gemm_file)
-    filtered_df = untunedf.drop_duplicates().reset_index(drop=True)
-    return filtered_df
-
-
-def get_tuned_gemm_list(tuned_gemm_file):
-    if os.path.exists(tuned_gemm_file):
-        tunedf = pd.read_csv(tuned_gemm_file)
-    else:
-        tunedf = pd.DataFrame(
-            columns=["cu_num", "M", "N", "K", "kernelId", "splitK", "us", "kernelName"]
-        )
-    return tunedf
 
 
 @perftest()
@@ -112,144 +89,113 @@ def generate_data(m, n, k, seed, device="cuda"):
     return x, weight_shuffle, x_scale_t, w_scale, out, weight, x_scale
 
 
-def tune_gemm_list(
-    untunedf, tunedf, issorted=False, useSplitK=False, mp_num=1, shape_grouped=False
-):
-    gpu = torch.cuda.current_device()
-    device_properties = torch.cuda.get_device_properties(gpu)
-    cu_num = device_properties.multi_processor_count
-    task = []
-    tasks_data = []
-    gemm_a8w8_data_idx = [0, 1, 2, 3, 4]
-    ref_data_idx = [0, 5, 6, 3]
-    seed = 0
-    for i in range(len(untunedf)):
-        M = untunedf.loc[i, "M"]
-        N = untunedf.loc[i, "N"]
-        K = untunedf.loc[i, "K"]
-        kernels_num = len(kernels_list)
-        if tunedf[
-            (tunedf["M"] == M)
-            & (tunedf["N"] == N)
-            & (tunedf["K"] == K)
-            & (tunedf["cu_num"] == cu_num)
-        ].empty:
-            seed = seed + 1
-            total_kernel_nums = 0
-            for i in range(kernels_num):
-                kernel = kernels_list[i]
-                maxsplitK = (
-                    aiter.compute_gemm_SplitK(
-                        M, N, K, kernel.MPerBLOCK, kernel.NPerBLOCK, kernel.KPerBLOCK
-                    )
-                    if useSplitK
-                    else 0
-                )
-                for splitK in range(maxsplitK + 1):
-                    info = ((cu_num, M, N, K), i, splitK)
-                    task.append(
-                        (
-                            info,
-                            generate_data,
-                            (M, N, K, seed),
-                            run_gemm_a8w8_blockscale_bpreshuffle,
-                            (
-                                gemm_a8w8_data_idx,
-                                i,
-                                splitK,
-                            ),
-                            {},
-                            run_torch,
-                            (ref_data_idx, None, dtypes.bf16),
-                            {},
-                            None,
-                            1e-2,
-                            0.1,
+class GemmA8W8BlockscaleBPreShuffleTuner(GemmCommonTuner):
+    ARG_DEFAULTS = {
+        **GemmCommonTuner.ARG_DEFAULTS,
+        "tune_file": "aiter/configs/a8w8_blockscale_bpreshuffle_tuned_gemm.csv",
+        "untune_file": "aiter/configs/a8w8_blockscale_bpreshuffle_untuned_gemm.csv",
+    }
+
+    def _setup_specific_arguments(self):
+        pass
+
+    def calculate(self, results, bpes=(1, 1, 2)):
+        ## bpes = (inbpe, w_bpe, outbpe)
+        return super().calculate(results, bpes=bpes)
+
+    def getKernelName(self, kernelId):
+        if kernelId < 0 or kernelId >= len(kernels_list):
+            return None
+        return kernels_list[kernelId].name
+
+    def tune(self, untunedf, tunedf, args):
+        issorted = args.sort
+        useSplitK = args.splitK
+        mp_num = args.mp
+        shape_grouped = True
+        errRatio = args.errRatio
+        cu_num = self.get_cu_num()
+        task = []
+        tasks_data = []  # [(kernel_nums, datas)]
+        seed = 10000
+
+        for i in range(len(untunedf)):
+            M = untunedf.loc[i, "M"]
+            N = untunedf.loc[i, "N"]
+            K = untunedf.loc[i, "K"]
+            kernels_num = len(kernels_list)
+            gemm_a8w8_idx = [0, 1, 2, 3, 4]  # input index in generate_data
+            ref_data_idx = [0, 5, 6, 3]
+
+            if tunedf[
+                (tunedf["M"] == M)
+                & (tunedf["N"] == N)
+                & (tunedf["K"] == K)
+                & (tunedf["cu_num"] == cu_num)
+            ].empty:
+                seed = seed + 1
+                total_kernel_nums = 0
+                for j in range(kernels_num):  # Use 'j' instead of 'i' to avoid conflict
+                    kernel = kernels_list[j]
+                    maxsplitK = (
+                        aiter.compute_gemm_SplitK(
+                            M,
+                            N,
+                            K,
+                            kernel.MPerBLOCK,
+                            kernel.NPerBLOCK,
+                            kernel.KPerBLOCK,
                         )
+                        if useSplitK
+                        else 0
                     )
-                    total_kernel_nums = total_kernel_nums + 1
+                    for splitK in range(maxsplitK + 1):
+                        info = ((cu_num, M, N, K), j, splitK, "")
+                        task.append(
+                            (
+                                info,
+                                generate_data,
+                                (M, N, K, seed),
+                                run_gemm_a8w8_blockscale_bpreshuffle,
+                                (
+                                    gemm_a8w8_idx,
+                                    j,
+                                    splitK,
+                                ),
+                                {},
+                                run_torch,
+                                (
+                                    ref_data_idx,
+                                    None,
+                                    dtypes.bf16,
+                                ),
+                                {},
+                                None,
+                                1e-2,
+                                0.1,
+                            )
+                        )
+                        total_kernel_nums = total_kernel_nums + 1
 
-            tasks_data.append((total_kernel_nums, ()))
-    if task:
-        ret = mp_tuner(task, tasks_data, mp_num, False, shape_grouped)
-        for el in ret:
-            info, time, err_ratio = el
-            (cu_num, M, N, K), kernelId, splitK = info
-            kernelName = (
-                "None"
-                if kernelId == -1 or time == "nan"
-                else kernels_list[kernelId].name
-            )
-            print(
-                f"Tuning result for M:{M}, N:{N}, K:{K}, cu_num:{cu_num} is kernelId={kernelId} {kernels_list[kernelId].name} {splitK=}, {time}us"
-            )
-            temp = pd.DataFrame(
-                {
-                    "M": [M],
-                    "N": [N],
-                    "K": [K],
-                    "cu_num": [cu_num],
-                    "kernelId": [kernelId],
-                    "splitK": [splitK],
-                    "us": [time],
-                    "kernelName": [kernelName],
-                }
-            )
-            tunedf = pd.concat([tunedf, temp], ignore_index=True)
+                tasks_data.append((total_kernel_nums, ()))
+            else:
+                print(f"M:{M}, N:{N}, K:{K} is in tuned gemm, skip!!!")
+                print()
+                print()
 
-        else:
-            print(f"M:{M}, N:{N}, K{K} is in tuned gemm, skip!!!")
-        print()
-        print()
-    if issorted:
-        tunedf = tunedf.sort_values(by=["cu_num", "M", "N", "K"])
-    print("Totall tuning result:")
-    print(tunedf)
-    return tunedf
+        ret = []
+        if task:
+            ret = mp_tuner(task, tasks_data, mp_num, False, shape_grouped, errRatio)
+
+        return ret
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog="generate",
-        description="gen API for CK gemm a8w8 kernel",
+    ## use default key and resultList
+    tuner = GemmA8W8BlockscaleBPreShuffleTuner(
+        "GemmA8W8BlockscaleBPreShuffleTuner",
+        description="gen API for CK gemm a8w8 blockscale bpreshuffle kernel",
     )
 
-    parser.add_argument(
-        "-i",
-        "--untune_file",
-        default="aiter/configs/a8w8_blockscale_bpreshuffle_untuned_gemm.csv",
-        required=False,
-        help="input",
-    )
-
-    parser.add_argument(
-        "--mp",
-        type=int,
-        default=torch.cuda.device_count(),
-        help="Tuning on multiple GPUs using multiple processes",
-    )
-
-    parser.add_argument(
-        "-o",
-        "--tune_file",
-        default="aiter/configs/a8w8_blockscale_bpreshuffle_tuned_gemm.csv",
-        required=False,
-        help="output: tuning result store this file",
-    )
-
-    parser.add_argument(
-        "-k", "--splitK", action="store_true", required=False, help="Use splitK kernels"
-    )
-
-    parser.add_argument(
-        "--sort",
-        action="store_true",
-        required=False,
-        help="Arranged according to the M N K size",
-    )
-
-    args = parser.parse_args()
-    untunedf = get_untuned_gemm_list(args.untune_file)
-    tunedf = get_tuned_gemm_list(args.tune_file)
-    tunedf = tune_gemm_list(untunedf, tunedf, args.sort, args.splitK, args.mp)
-    tunedf.to_csv(args.tune_file, index=False, na_rep="Nan")
+    args = tuner.parse_args()
+    tuner.run(args, False)

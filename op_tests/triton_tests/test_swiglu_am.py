Fixed draft to make sure swiglu and softmax implementations closed with torch functions

op_tests/triton_tests/test_swiglu_am.py
@@ -5,6 +5,12 @@ import pytest
 from aiter.ops.triton.swiglu_am import swiglu
 from aiter.ops.triton.utils.types import str_to_torch_dtype
 
+import torch.nn as nn
+
+
+def torch_swiglu(x):
+    return x * torch.sigmoid(x) * x
+
 
 # pytest
 @pytest.mark.parametrize("dtype", ["fp32", "fp16", "bf16"])
@@ -28,13 +34,11 @@ def test_swiglu(M, N, dtype):
     torch.manual_seed(0)
     x = torch.randn(M, N, dtype=dtype, device="cuda")
     y_triton = swiglu(x)
-    y_torch = torch.swiglu(x, axis=1)
+    y_torch = torch_swiglu(x)
 
-    if dtype in (torch.float16, torch.bfloat16):
-        atol, rtol = 1e-2, 1e-2
-    else:
-        # float32 typically can be tighter
-        atol, rtol = 1e-5, 1e-5
+    atol, rtol = 1e-5, 1e-5  # triton sigmoid apparently can't handle fp16
 
     torch.testing.assert_close(y_triton, y_torch, atol=atol, rtol=rtol)
     print("swiglu asserted")
+
+test_swiglu(8192, 8192, "fp32")
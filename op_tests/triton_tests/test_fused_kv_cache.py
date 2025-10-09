8ad8379ffd1d0bbb6da682e7b3bf989468a36b56
Aliasger Zaidy <aliasger.zaidy@amd.com>
2025-10-09 14:43:44 +0000

    Catchall PR for all 355_wip related changes
    Co-authored-by: Shao-Chun Lee <Shao-Chun.Lee@amd.com>
    Co-authored-by:  Mehmet Cagri <mehmet.kaymak@amd.com>

--- op_tests/triton_tests/test_fused_kv_cache.py
+++ op_tests/triton_tests/test_fused_kv_cache.py
@@ -6,24 +6,24 @@ import pytest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from test_rope import ref_rope_sbhd_fwd, RotateStyle
 from .test_rope import generate_rope_inputs
-from aiter.ops.triton.fused_kv_cache import fused_qk_rope_cosine_cache_llama
+from aiter.ops.triton.fused_kv_cache import fused_qk_rope_reshape_and_cache
 from aiter.ops.triton.utils._triton import arch_info
 
 
-@pytest.mark.parametrize("T", [1, 2, 4, 128])
+@pytest.mark.parametrize("T", [1, 2, 4, 2048, 8192])
 @pytest.mark.parametrize("QH_per_KH", [1, 4, 16])
 @pytest.mark.parametrize("KH", [1, 8])
 @pytest.mark.parametrize("D", [64, 128])  # For now, D is power of 2. D >= 16
-@pytest.mark.parametrize("num_kv_cahce_tokens", [8193])
-@pytest.mark.parametrize("rotate_style", [RotateStyle.GPTJ])
-@pytest.mark.parametrize("reuse_freqs_front_part", [True])
-@pytest.mark.parametrize("cache_dtype", [torch.bfloat16])
+@pytest.mark.parametrize("num_kv_cahce_tokens", [16384])
+@pytest.mark.parametrize("rotate_style", [RotateStyle.GPTJ, RotateStyle.NEOX])
+@pytest.mark.parametrize("reuse_freqs_front_part", [False, True])
+@pytest.mark.parametrize("cache_dtype", [torch.bfloat16, torch.uint8])
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
-@pytest.mark.parametrize("cache_flash", [True])
+@pytest.mark.parametrize("cache_flash", [False, True])
 @pytest.mark.parametrize("block_size", [16])
-@pytest.mark.parametrize("x_size", [8])  # not used
-@pytest.mark.parametrize("offs", [False])
-def test_fused_qk_rope_cosine_cache_llama(
+@pytest.mark.parametrize("x_size", [8, 16])
+@pytest.mark.parametrize("offs", [False, True])
+def test_fused_qk_rope_reshape_and_cache(
     T: int,
     QH_per_KH: int,
     KH: int,
@@ -64,14 +64,20 @@ def test_fused_qk_rope_cosine_cache_llama(
 
     if cache_flash:
         key_cache = torch.zeros(
-            (T, num_kv_cahce_tokens, KH, D), dtype=cache_dtype, device="cuda"
+            (num_kv_cahce_tokens, block_size, KH, D), dtype=cache_dtype, device="cuda"
         )
         value_cache = torch.zeros(
-            (T, num_kv_cahce_tokens, KH, D), dtype=cache_dtype, device="cuda"
+            (num_kv_cahce_tokens, block_size, KH, D), dtype=cache_dtype, device="cuda"
         )
     else:
-        pytest.skip()
-
+        key_cache = torch.zeros(
+            (num_kv_cahce_tokens, KH, D // x_size, block_size, x_size),
+            dtype=cache_dtype,
+            device="cuda",
+        )
+        value_cache = torch.zeros(
+            (num_kv_cahce_tokens, KH, D, block_size), dtype=cache_dtype, device="cuda"
+        )
     if cache_dtype == torch.uint8:
         k_scale = torch.randn(
             [
@@ -103,7 +109,6 @@ def test_fused_qk_rope_cosine_cache_llama(
             device="cuda",
         )[0]
     slot_mapping = torch.randperm(T, device="cuda")
-    positions = slot_mapping
     key_cache_og_dtype = key_cache.dtype
     value_cache_og_dtype = value_cache.dtype
 
@@ -132,10 +137,9 @@ def test_fused_qk_rope_cosine_cache_llama(
 
     torch_key_cache = key_cache.clone()
     torch_value_cache = value_cache.clone()
-    # slot_t = slot_mapping // block_size
-    # slot_b = slot_mapping % block_size
-    slot_t = torch.arange(slot_mapping.shape[0]).to(slot_mapping.device)
-    slot_b = slot_mapping
+    slot_t = slot_mapping // block_size
+    slot_b = slot_mapping % block_size
+    torch_k_og_dtype = torch_k.clone()
     if cache_dtype == torch.uint8:
         torch_key_cache = torch_key_cache.view(cache_dtype_actual)
         torch_value_cache = torch_value_cache.view(cache_dtype_actual)
@@ -143,10 +147,15 @@ def test_fused_qk_rope_cosine_cache_llama(
         torch_v = (v.to(torch.float32) / v_scale).to(cache_dtype_actual)
     else:
         torch_v = v
+    torch_zeros = torch.zeros_like(q)
     if cache_flash:
         torch_key_cache[slot_t, slot_b] = torch_k
         torch_value_cache[slot_t, slot_b] = torch_v
-
+    else:
+        torch_key_cache[slot_t, :, :, slot_b, :] = torch_k.reshape(
+            T, KH, D // x_size, x_size
+        )
+        torch_value_cache[slot_t, :, :, slot_b] = torch_v
     torch_key_cache = torch_key_cache.view(key_cache_og_dtype)
     torch_value_cache = torch_value_cache.view(value_cache_og_dtype)
 
@@ -155,28 +164,33 @@ def test_fused_qk_rope_cosine_cache_llama(
     if cache_dtype == torch.uint8:
         triton_key_cache = triton_key_cache.view(cache_dtype_actual)
         triton_value_cache = triton_value_cache.view(cache_dtype_actual)
-    triton_q, triton_key_cache, triton_value_cache = fused_qk_rope_cosine_cache_llama(
-        q,
-        k,
-        v,
-        triton_key_cache,
-        triton_value_cache,
-        slot_mapping,
-        positions,
-        cos,
-        sin,
-        k_scale,
-        v_scale,
-        (rotate_style == RotateStyle.NEOX),
-        flash_layout=cache_flash,
-        apply_scale=(cache_dtype != torch.bfloat16),
-        offs=offsets,
-        q_out=q,
+    triton_q, triton_k, triton_key_cache, triton_value_cache, triton_zeros = (
+        fused_qk_rope_reshape_and_cache(
+            q,
+            k,
+            v,
+            triton_key_cache,
+            triton_value_cache,
+            slot_mapping,
+            positions,
+            cos,
+            sin,
+            k_scale,
+            v_scale,
+            (rotate_style == RotateStyle.NEOX),
+            flash_layout=cache_flash,
+            apply_scale=(cache_dtype != torch.bfloat16),
+            offs=offsets,
+            q_out=q,
+            k_out=k,
+        )
     )
     triton_key_cache = triton_key_cache.view(key_cache_og_dtype)
     triton_value_cache = triton_value_cache.view(value_cache_og_dtype)
 
     torch.testing.assert_close(torch_q, triton_q, atol=1e-1, rtol=1e-1)
+    torch.testing.assert_close(torch_k_og_dtype, triton_k, atol=1e-1, rtol=1e-1)
+    torch.testing.assert_close(torch_zeros, triton_zeros, atol=0.1, rtol=0.1)
 
     if cache_dtype == torch.uint8:
         torch_key_cache = torch_key_cache.view(cache_dtype_actual).to(dtype)
@@ -194,6 +208,19 @@ def test_fused_qk_rope_cosine_cache_llama(
         torch.testing.assert_close(
             torch_value_cache[slot_t, slot_b],
             triton_value_cache[slot_t, slot_b],
+            atol=1e-1,
+            rtol=1e-1,
+        )
+    else:
+        torch.testing.assert_close(
+            torch_key_cache[slot_t, :, :, slot_b, :],
+            triton_key_cache[slot_t, :, :, slot_b, :],
+            atol=1e-1,
+            rtol=1e-1,
+        )
+        torch.testing.assert_close(
+            torch_value_cache[slot_t, :, :, slot_b],
+            triton_value_cache[slot_t, :, :, slot_b],
             atol=1e-1,
             rtol=1e-1,
         )

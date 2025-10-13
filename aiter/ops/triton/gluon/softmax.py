aea1a8d2ef36a1394cbf2a430ee3b07631c92667
eky-amd <ethan.ky@amd.com>
2025-10-13 23:19:32 +0000

    draft of async pipeline softmax

--- aiter/ops/triton/gluon/softmax.py
+++ aiter/ops/triton/gluon/softmax.py
@@ -16,6 +16,87 @@ _LOGGER = AiterTritonLogger()
 import triton.language as tl
 from triton.experimental import gluon
 from triton.experimental.gluon import language as gl
+from triton.experimental.gluon.language.amd.cdna4 import async_copy as acp
+
+@gluon.jit
+def _issue_loads(
+    copy_idx, 
+    cols_smem, 
+    col_offsets_range,
+    row_start_ptr, 
+    n_cols,
+    BLOCK_SIZE: gl.constexpr, 
+    NUM_STAGES: gl.constexpr
+):
+    col_offsets = copy_idx * BLOCK_SIZE + col_offsets_range
+    mask = col_offsets < n_cols
+    
+    acp.buffer_load_to_shared(
+        cols_smem.index(copy_idx % NUM_STAGES), row_start_ptr, col_offsets, mask, other=-float("inf"), cache_modifier=".cg"
+    )
+    return copy_idx + 1
+
+
+@gluon.jit
+def _perform_loop1(
+    m,
+    row_sum,
+    read_idx, 
+    cols_smem, 
+    layout: gl.constexpr,
+    NUM_STAGES: gl.constexpr
+):
+    row_block = cols_smem.index(read_idx % NUM_STAGES).load(layout)
+    
+    # find the max within the block
+    m_p = gl.max(row_block, axis=0)
+        
+    # find new max among all blocks
+    m_p = gl.maximum(m, m_p)
+    
+    # correct previous row sum
+    row_sum = row_sum * gl.exp(m - m_p)
+    
+    # add new exponential to row sum
+    row_sum += gl.sum(
+        gl.exp(row_block - m_p),
+        axis=0
+    )
+    
+    # save the new max and update block
+    m = m_p
+    
+    return m, row_sum, read_idx + 1
+
+
+@gluon.jit
+def _perform_loop2(
+    m,
+    row_sum,
+    read_idx, 
+    cols_smem, 
+    col_offsets_range,
+    output_row_start_ptr,
+    n_cols,
+    output_dtype,
+    layout: gl.constexpr,
+    BLOCK_SIZE: gl.constexpr,
+    NUM_STAGES: gl.constexpr
+):
+    col_offsets = read_idx * BLOCK_SIZE + col_offsets_range
+    mask = col_offsets < n_cols
+    row_block = cols_smem.index(read_idx % NUM_STAGES).load(layout)
+    
+    # subtract, exponentiate and divide by sum
+    softmax_output = gl.exp(row_block - m) / row_sum
+    softmax_output = softmax_output.to(output_dtype)
+    
+    # store in output array
+    gl.amd.cdna4.buffer_store(
+        stored_value=softmax_output, ptr=output_row_start_ptr, offsets=col_offsets, mask=mask, cache=".cg"
+    )
+    
+    return read_idx + 1
 
 
 @gluon.jit
@@ -43,6 +124,11 @@ def _softmax_kernel_online(
         warps_per_cta=[gl.num_warps()],
         order=[0],
     )
+    shared_cols: gl.constexpr = gl.SwizzledSharedLayout(vec=1, per_phase=1, max_phase=1, order=[0])
+    cols_smem = gl.allocate_shared_memory(input_ptr.type.element_ty, [NUM_STAGES, BLOCK_SIZE], layout=shared_cols)
+    copy_idx = 0
+    read_idx = 0
+    
     col_offsets_range = gl.arange(0, BLOCK_SIZE, layout=blocked_cols)
     
     # loop 1: find the max and sum of each row
@@ -50,87 +136,62 @@ def _softmax_kernel_online(
     row_sum = 0.0
     row_start_ptr = input_ptr + row_idx * input_row_stride
     
-    # prologue
-    start_col_offsets = col_offsets_range
-    start_mask = start_col_offsets < n_cols
-    start_row_block = gl.amd.cdna4.buffer_load(
-        ptr=row_start_ptr, offsets=start_col_offsets, mask=start_mask, other=-float("inf"), cache=".cg"
-    )
-    
-    # iterate through blocks of columns
-    col_offsets = start_col_offsets
-    mask = start_mask
-    row_block = start_row_block
-    for b in tl.range(BLOCK_SIZE, n_cols, BLOCK_SIZE):
-        # get column offsets from row starting pointer
-        col_offsets = b + col_offsets_range
-        
-        # create mask to ensure in-bounds offsets
-        mask = col_offsets < n_cols
-        
-        # load next block of columns of row from global memory
-        next_row_block = gl.amd.cdna4.buffer_load(
-            ptr=row_start_ptr, offsets=col_offsets, mask=mask, other=-float("inf"), cache=".cg"
+    # prefill the pipeline
+    for _ in gl.static_range(NUM_STAGES - 1):
+        copy_idx = _issue_loads(
+            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
         )
         
-        # find the max within the block
-        m_p = gl.max(row_block, axis=0)
-        
-        # find new max among all blocks
-        m_p = gl.maximum(m, m_p)
-        
-        # correct previous row sum
-        row_sum = row_sum * gl.exp(m - m_p)
-        
-        # add new exponential to row sum
-        row_sum += gl.sum(
-            gl.exp(row_block - m_p),
-            axis=0
+    # steady state
+    for _ in range(gl.cdiv(n_cols, BLOCK_SIZE) - (NUM_STAGES - 1)):
+        # issue the overlapping copy
+        copy_idx = _issue_loads(
+            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
+        )
+
+        # wait for NUM_STAGES - 1 copies to finish before doing any computation
+        acp.async_wait(NUM_STAGES - 1)
+        m, row_sum, read_idx = _perform_loop1(
+            m, row_sum, read_idx, cols_smem, blocked_cols, NUM_STAGES
         )
         
-        # save the new max and update block
-        m = m_p
-        row_block = next_row_block
-        
-    # epilogue
-    m_p = gl.max(row_block, axis=0)
-    m_p = gl.maximum(m, m_p)
-    row_sum = row_sum * gl.exp(m - m_p)
-    row_sum += gl.sum(
-        gl.exp(row_block - m_p),
-        axis=0
-    )
-    m = m_p
+    # finish the pipeline
+    for i in gl.static_range(NUM_STAGES - 1):
+        acp.async_wait(NUM_STAGES - 1 - i)
+        m, row_sum, read_idx = _perform_loop1(
+            m, row_sum, read_idx, cols_smem, blocked_cols, NUM_STAGES
+        )
     
-    # loop 2: divide each row by respective norms
+    # loop 2: divide each row by respective norms, and then store
     output_row_start_ptr = output_ptr + row_idx * output_row_stride
-    col_offsets = start_col_offsets
-    mask = start_mask
-    row_block = start_row_block
-    for b in tl.range(BLOCK_SIZE, n_cols, BLOCK_SIZE):
-        next_col_offsets = b + gl.arange(0, BLOCK_SIZE, layout=blocked_cols)
-        next_mask = col_offsets < n_cols
-        next_row_block = gl.amd.cdna4.buffer_load(
-            ptr=row_start_ptr, offsets=next_col_offsets, mask=next_mask, other=-float("inf"), cache=".cg"
+    copy_idx = 0
+    read_idx = 0
+    
+    # prefill the pipeline
+    for _ in gl.static_range(NUM_STAGES - 1):
+        copy_idx = _issue_loads(
+            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
         )
         
-        # subtract, exponentiate and divide by sum
-        softmax_output = gl.exp(row_block - m) / row_sum
-        softmax_output = softmax_output.to(output_ptr.type.element_ty)
-        
-        # store in output array
-        gl.amd.cdna4.buffer_store(
-            stored_value=softmax_output, ptr=output_row_start_ptr, offsets=col_offsets, mask=mask, cache=".cg"
+    # steady state
+    for _ in range(gl.cdiv(n_cols, BLOCK_SIZE) - (NUM_STAGES - 1)):
+        # issue the overlapping copy
+        copy_idx = _issue_loads(
+            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
+        )
+
+        # wait for NUM_STAGES - 1 copies to finish before doing any computation
+        acp.async_wait(NUM_STAGES - 1)
+        read_idx = _perform_loop2(
+            m, row_sum, read_idx, cols_smem, col_offsets_range, output_row_start_ptr, n_cols, output_ptr.type.element_ty, blocked_cols, BLOCK_SIZE, NUM_STAGES
         )
         
-        col_offsets = next_col_offsets
-        mask = next_mask
-        row_block = next_row_block
-    softmax_output = gl.exp(row_block - m) / row_sum
-    softmax_output = softmax_output.to(output_ptr.type.element_ty)
-    gl.amd.cdna4.buffer_store(
-        stored_value=softmax_output, ptr=output_row_start_ptr, offsets=col_offsets, mask=mask, cache=".cg"
-    )
+    # finish the pipeline
+    for i in gl.static_range(NUM_STAGES - 1):
+        acp.async_wait(NUM_STAGES - 1 - i)
+        read_idx = _perform_loop2(
+            m, row_sum, read_idx, cols_smem, col_offsets_range, output_row_start_ptr, n_cols, output_ptr.type.element_ty, blocked_cols, BLOCK_SIZE, NUM_STAGES
+        )
 
 
 def softmax(x):

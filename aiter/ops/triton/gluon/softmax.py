9d04ba3d66fce7810e42cefef9745b189e72436c
eky-amd <ethan.ky@amd.com>
2025-10-31 22:52:20 +0000

    attempt at fixing 128 or 32 bit thread sizes

--- aiter/ops/triton/gluon/softmax.py
+++ aiter/ops/triton/gluon/softmax.py
@@ -18,54 +18,54 @@ from triton.experimental import gluon
 from triton.experimental.gluon import language as gl
 from triton.experimental.gluon.language.amd.cdna4 import async_copy as acp
 
+
 @gluon.jit
 def _issue_loads(
-    copy_idx, 
-    cols_smem, 
-    col_offsets_range,
-    row_start_ptr, 
+    copy_idx,
+    cols_smem,
+    row_start_ptr,
     n_cols,
-    BLOCK_SIZE: gl.constexpr, 
-    NUM_STAGES: gl.constexpr
+    layout: gl.constexpr,
+    BLOCK_SIZE: gl.constexpr,
+    NUM_STAGES: gl.constexpr,
 ):
-    col_offsets = copy_idx * BLOCK_SIZE + col_offsets_range
+    col_offsets = copy_idx * BLOCK_SIZE + gl.arange(0, BLOCK_SIZE, layout=layout)
     mask = col_offsets < n_cols
-    
-    acp.buffer_load_to_shared(
-        cols_smem.index(copy_idx % NUM_STAGES), row_start_ptr, col_offsets, mask, other=-float("inf"), cache_modifier=".cg"
+
+    # acp.buffer_load_to_shared(
+    #     cols_smem.index(copy_idx % NUM_STAGES), row_start_ptr, col_offsets, mask, other=-float("inf"), cache_modifier=".cg"
+    # )
+    acp.global_load_to_shared(
+        cols_smem.index(copy_idx % NUM_STAGES),
+        row_start_ptr + col_offsets,
+        mask=mask,
+        other=-float("inf"),
+        cache_modifier=".cg",
     )
     return copy_idx + 1
 
 
 @gluon.jit
 def _perform_loop1(
-    m,
-    row_sum,
-    read_idx, 
-    cols_smem, 
-    layout: gl.constexpr,
-    NUM_STAGES: gl.constexpr
+    m, row_sum, read_idx, cols_smem, layout: gl.constexpr, NUM_STAGES: gl.constexpr
 ):
     row_block = cols_smem.index(read_idx % NUM_STAGES).load(layout)
-    
+
     # find the max within the block
     m_p = gl.max(row_block, axis=0)
-        
+
     # find new max among all blocks
     m_p = gl.maximum(m, m_p)
-    
+
     # correct previous row sum
     row_sum = row_sum * gl.exp(m - m_p)
-    
+
     # add new exponential to row sum
-    row_sum += gl.sum(
-        gl.exp(row_block - m_p),
-        axis=0
-    )
-    
+    row_sum += gl.sum(gl.exp(row_block - m_p), axis=0)
+
     # save the new max and update block
     m = m_p
-    
+
     return m, row_sum, read_idx + 1
 
 
@@ -73,29 +73,32 @@ def _perform_loop1(
 def _perform_loop2(
     m,
     row_sum,
-    read_idx, 
-    cols_smem, 
-    col_offsets_range,
+    read_idx,
+    cols_smem,
     output_row_start_ptr,
     n_cols,
     output_dtype,
     layout: gl.constexpr,
     BLOCK_SIZE: gl.constexpr,
-    NUM_STAGES: gl.constexpr
+    NUM_STAGES: gl.constexpr,
 ):
-    col_offsets = read_idx * BLOCK_SIZE + col_offsets_range
+    col_offsets = read_idx * BLOCK_SIZE + gl.arange(0, BLOCK_SIZE, layout=layout)
     mask = col_offsets < n_cols
     row_block = cols_smem.index(read_idx % NUM_STAGES).load(layout)
-    
+
     # subtract, exponentiate and divide by sum
     softmax_output = gl.exp(row_block - m) / row_sum
     softmax_output = softmax_output.to(output_dtype)
-    
+
     # store in output array
     gl.amd.cdna4.buffer_store(
-        stored_value=softmax_output, ptr=output_row_start_ptr, offsets=col_offsets, mask=mask, cache=".cg"
+        stored_value=softmax_output,
+        ptr=output_row_start_ptr,
+        offsets=col_offsets,
+        mask=mask,
+        cache=".cg",
     )
-    
+
     return read_idx + 1
 
 
@@ -115,82 +118,123 @@ def _softmax_kernel_online(
     row_start = gl.program_id(0)
     row_idx = row_start
 
-    gl.static_assert(
-        SIZE_PER_THREAD <= triton.cdiv(BLOCK_SIZE, gl.num_warps() * THREADS_PER_WARP)
-    )
     blocked_cols: gl.constexpr = gl.BlockedLayout(
         size_per_thread=[SIZE_PER_THREAD],
         threads_per_warp=[THREADS_PER_WARP],
         warps_per_cta=[gl.num_warps()],
         order=[0],
     )
-    shared_cols: gl.constexpr = gl.SwizzledSharedLayout(vec=1, per_phase=1, max_phase=1, order=[0])
-    cols_smem = gl.allocate_shared_memory(input_ptr.type.element_ty, [NUM_STAGES, BLOCK_SIZE], layout=shared_cols)
+    shared_cols: gl.constexpr = gl.SwizzledSharedLayout(
+        vec=1, per_phase=1, max_phase=1, order=[0]
+    )
+    cols_smem = gl.allocate_shared_memory(
+        input_ptr.type.element_ty, [NUM_STAGES, BLOCK_SIZE], layout=shared_cols
+    )
     copy_idx = 0
     read_idx = 0
-    
-    col_offsets_range = gl.arange(0, BLOCK_SIZE, layout=blocked_cols)
 
     # loop 1: find the max and sum of each row
     m = -float("inf")
     row_sum = 0.0
     row_start_ptr = input_ptr + row_idx * input_row_stride
-    
+
     # prefill the pipeline
     for _ in gl.static_range(NUM_STAGES - 1):
         copy_idx = _issue_loads(
-            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
+            copy_idx,
+            cols_smem,
+            row_start_ptr,
+            n_cols,
+            blocked_cols,
+            BLOCK_SIZE,
+            NUM_STAGES,
         )
-        
+
     # steady state
     for _ in range(gl.cdiv(n_cols, BLOCK_SIZE) - (NUM_STAGES - 1)):
         # issue the overlapping copy
         copy_idx = _issue_loads(
-            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
+            copy_idx,
+            cols_smem,
+            row_start_ptr,
+            n_cols,
+            blocked_cols,
+            BLOCK_SIZE,
+            NUM_STAGES,
         )
 
-        # wait for NUM_STAGES - 1 copies to finish before doing any computation
+        # wait for a copy to finish before doing any computation
         acp.async_wait(NUM_STAGES - 1)
         m, row_sum, read_idx = _perform_loop1(
             m, row_sum, read_idx, cols_smem, blocked_cols, NUM_STAGES
         )
-        
+
     # finish the pipeline
     for i in gl.static_range(NUM_STAGES - 1):
         acp.async_wait(NUM_STAGES - 1 - i)
         m, row_sum, read_idx = _perform_loop1(
             m, row_sum, read_idx, cols_smem, blocked_cols, NUM_STAGES
         )
-    
+
     # loop 2: divide each row by respective norms, and then store
     output_row_start_ptr = output_ptr + row_idx * output_row_stride
     copy_idx = 0
     read_idx = 0
-    
+
     # prefill the pipeline
     for _ in gl.static_range(NUM_STAGES - 1):
         copy_idx = _issue_loads(
-            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
+            copy_idx,
+            cols_smem,
+            row_start_ptr,
+            n_cols,
+            blocked_cols,
+            BLOCK_SIZE,
+            NUM_STAGES,
         )
-        
+
     # steady state
     for _ in range(gl.cdiv(n_cols, BLOCK_SIZE) - (NUM_STAGES - 1)):
         # issue the overlapping copy
         copy_idx = _issue_loads(
-            copy_idx, cols_smem, col_offsets_range, row_start_ptr, n_cols, BLOCK_SIZE, NUM_STAGES
+            copy_idx,
+            cols_smem,
+            row_start_ptr,
+            n_cols,
+            blocked_cols,
+            BLOCK_SIZE,
+            NUM_STAGES,
         )
 
-        # wait for NUM_STAGES - 1 copies to finish before doing any computation
+        # wait for a copy to finish before doing any computation
         acp.async_wait(NUM_STAGES - 1)
         read_idx = _perform_loop2(
-            m, row_sum, read_idx, cols_smem, col_offsets_range, output_row_start_ptr, n_cols, output_ptr.type.element_ty, blocked_cols, BLOCK_SIZE, NUM_STAGES
+            m,
+            row_sum,
+            read_idx,
+            cols_smem,
+            output_row_start_ptr,
+            n_cols,
+            output_ptr.type.element_ty,
+            blocked_cols,
+            BLOCK_SIZE,
+            NUM_STAGES,
         )
-        
+
     # finish the pipeline
     for i in gl.static_range(NUM_STAGES - 1):
         acp.async_wait(NUM_STAGES - 1 - i)
         read_idx = _perform_loop2(
-            m, row_sum, read_idx, cols_smem, col_offsets_range, output_row_start_ptr, n_cols, output_ptr.type.element_ty, blocked_cols, BLOCK_SIZE, NUM_STAGES
+            m,
+            row_sum,
+            read_idx,
+            cols_smem,
+            output_row_start_ptr,
+            n_cols,
+            output_ptr.type.element_ty,
+            blocked_cols,
+            BLOCK_SIZE,
+            NUM_STAGES,
         )
 
 
@@ -219,13 +263,20 @@ def softmax(x):
     num_warps = 8
     num_stages = 2
 
-    buffer_size = 16
+    buffer_size_128 = 16
+    buffer_size_32 = 4
     threads_per_warp = 64
 
-    # each thread should only be loading as many elements
-    # as can fit in the load buffer (determined by element size)
+    # Each thread should only be loading as many elements as can fit in the load buffer (determined by element size)
+    # We also need to make sure that either size_per_thread * (x.element_size() * 8) == 128 or 32,
+    # for async_copy load functions
     cols_per_thread = triton.cdiv(BLOCK_SIZE, num_warps * threads_per_warp)
-    size_per_thread = min(cols_per_thread, buffer_size // x.element_size())
+    size_per_thread = min(cols_per_thread, buffer_size_128 // x.element_size())
+    if (
+        size_per_thread * x.element_size() != buffer_size_128
+        and size_per_thread * x.element_size() != buffer_size_32
+    ):
+        size_per_thread = buffer_size_32 // x.element_size()
 
     num_programs = n_rows
 

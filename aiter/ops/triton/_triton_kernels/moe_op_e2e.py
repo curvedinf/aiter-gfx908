9e06e209782a50d43fcbe239e367b8de06abcc69
Juuso Korhonen <40278371+juuso-oskari@users.noreply.github.com>
2025-11-03 12:06:09 +0000

    produce pass manager error for debug

--- aiter/ops/triton/_triton_kernels/moe_op_e2e.py
+++ aiter/ops/triton/_triton_kernels/moe_op_e2e.py
@@ -338,7 +338,8 @@ def e2e_moe_kernel(
     out_ptrs = Out + stride_om * offs_token[:, None] + offs_k2[None, :] * stride_ok
 
     num_k2 = tl.cdiv(K, BLOCK_SIZE_K2)
-    for k2 in tl.range(0, num_k2, num_stages=1 if use_fp8_w8a8 else None):
+    # TODO: add num_stages > 1 support for use_fp8_w8a8. Now produces a compile error.
+    for k2 in tl.range(0, num_k2):
         w2 = tl.load(
             w2_ptrs + k2 * BLOCK_SIZE_K2 * stride_w2k,
         )

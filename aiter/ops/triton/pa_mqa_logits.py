e71d0942954d67a7981f1329313c0cafbb9a4bd4
Feng Shijie <Shijie.Feng@amd.com>
2025-11-04 07:31:58 +0000

    Minor update

--- aiter/ops/triton/pa_mqa_logits.py
+++ aiter/ops/triton/pa_mqa_logits.py
@@ -336,7 +336,7 @@ def _compile_deepgemm_fp8_paged_mqa_logits(
             options=options,
         )
     else:
-        padded_str = "T" if is_padded_mode else "F"
+        padded_str = "T" if is_padded_mode and not Preshuffle else "F"
         kernel_str = f"paged_mqa_logits{"_preshuffle" if Preshuffle else ""}_{ChunkQ}x{ChunkK}x{HiddenDim}_B{KVBlockSize}P{padded_str}W{WavePerEU}"
         metadata_pth = f"{AITER_TRITON_CONFIGS_PATH}/paged_mqa_logits/aot/{kernel_str}"
         with AOTMetadataContext(
@@ -454,6 +454,7 @@ def deepgemm_fp8_paged_mqa_logits(
                 hidden_dim,
             )
     else:
+        assert not Preshuffle, "Preshuffle mode is only supported on gluon kernel."
         kernel = _deepgemm_fp8_paged_mqa_logits[grid](
             batch_size,
             next_n,

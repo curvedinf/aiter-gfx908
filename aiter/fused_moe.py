8274065aa373e3825b8ffee88fb83ce4800bdced
xudoyuan <xudoyuan@amd.com>
2025-11-05 09:16:19 +0000

    fp8 moe bugfix tuning rebase tuned_fmoe.csv

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -586,7 +586,7 @@ def get_2stage_cfgs(
             in fused_moe_1stage_dict[get_gfx()]
         ):
             if q_type == QuantType.per_1x128:
-                run_1stage = True and (inter_dim % 256 == 0) and (token > 64)
+                run_1stage = True and (inter_dim % 256 == 0)
             elif q_type == QuantType.per_Token and q_dtype_w in [dtypes.i8, dtypes.fp8]:
                 run_1stage = token > 32
             elif q_type != QuantType.per_1x32:
@@ -595,7 +595,7 @@ def get_2stage_cfgs(
             BLOCK_SIZE_M
             if run_1stage
             else (
-                16
+                64
                 if q_type == QuantType.per_1x128
                 else get_block_size_M(token, topk, expert, inter_dim)
             )
@@ -634,7 +634,6 @@ def get_2stage_cfgs(
             torch.uint32,
             dtypes.fp4x2,
         ]
-        or (q_type == QuantType.per_1x128 and block_m == 16)
     ):
         return MOEMetadata(
             functools.partial(

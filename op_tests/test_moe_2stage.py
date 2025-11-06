61130b6280af460d4eaaf98453169ed73d97fca1
xudoyuan <xudoyuan@amd.com>
2025-11-06 06:58:47 +0000

    reformat

--- op_tests/test_moe_2stage.py
+++ op_tests/test_moe_2stage.py
@@ -411,14 +411,13 @@ def test_fmoe(
             out2_aiter,
             msg=f"aiter_all_stages:{us_fuse:>8.2f} us......",
         )
-        
+
         def calc_diff(x: torch.Tensor, y: torch.Tensor):
             x, y = x.double(), y.double()
             denominator = (x * x + y * y).sum()
             sim = 2 * (x * y).sum() / denominator
             return 1 - sim
- 
- 
+
         logits_diff = calc_diff(out2_ref, out2_aiter)
         assert logits_diff < 1e-3
 

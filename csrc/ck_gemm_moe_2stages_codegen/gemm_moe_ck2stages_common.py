397482d16cbdb7933daa92f90cf8082ac26eaf82
zhimding <zhimding@amd.com>
2025-11-07 12:50:21 +0000

    v3 n128

--- csrc/ck_gemm_moe_2stages_codegen/gemm_moe_ck2stages_common.py
+++ csrc/ck_gemm_moe_2stages_codegen/gemm_moe_ck2stages_common.py
@@ -288,8 +288,8 @@ a4w4_gemm2_kernels_list= {
      1: kernelInstanceGEMM2(       256,        64,        128,       128,     1,       4,         3,),
      2: kernelInstanceGEMM2(       256,       128,        128,       128,     1,       4,         3,),
      4: kernelInstanceGEMM2(        64,        32,         64,       128,     1,       1,         1,),
-     5: kernelInstanceGEMM2(        64,        64,         64,       128,     1,       1,         1,),
-     6: kernelInstanceGEMM2(        64,       128,        128,       128,     1,       1,         1,),
+     5: kernelInstanceGEMM2(        64,        64,         128,       128,     1,       1,         3,),
+     6: kernelInstanceGEMM2(        64,       128,        128,       128,     1,       1,         3,),
     #  7: kernelInstanceGEMM2(      256,       256,         64,       128,     2,       2,         3,),
 }
 # gemm2 out:bf16/fp16 A:fp8 B:in4
--- csrc/ck_gemm_moe_2stages_codegen/gen_instances.py
+++ csrc/ck_gemm_moe_2stages_codegen/gen_instances.py
@@ -380,11 +380,11 @@ A4W4_gemm2_heuristic_dispatch = """
             }}
             else if (block_m == 64)
             {{
-                return ck_moe_stage2_gemm<{A0DataType}, {B0DataType}, {AccDataType}, {EDataType}, {CDEElementOp}, V1, 64, 64, 64, 128/sizeof({A0DataType}), 1, 1, {Nswizzle}, {Quant} == static_cast<int>(QuantType::per_Tensor), {MulRoutedWeight}, {ActOP}>;
+                return ck_moe_stage2_gemm<{A0DataType}, {B0DataType}, {AccDataType}, {EDataType}, {CDEElementOp}, V3, 64, 64, 128, 128/sizeof({A0DataType}), 1, 1, {Nswizzle}, {Quant} == static_cast<int>(QuantType::per_Tensor), {MulRoutedWeight}, {ActOP}>;
             }}
             else if (block_m == 128)
             {{
-                return ck_moe_stage2_gemm<{A0DataType}, {B0DataType}, {AccDataType}, {EDataType}, {CDEElementOp}, V1, 64, 128, 128, 128/sizeof({A0DataType}), 1, 1, {Nswizzle}, {Quant} == static_cast<int>(QuantType::per_Tensor), {MulRoutedWeight}, {ActOP}>;
+                return ck_moe_stage2_gemm<{A0DataType}, {B0DataType}, {AccDataType}, {EDataType}, {CDEElementOp}, V3, 64, 128, 128, 128/sizeof({A0DataType}), 1, 1, {Nswizzle}, {Quant} == static_cast<int>(QuantType::per_Tensor), {MulRoutedWeight}, {ActOP}>;
             }}
             else
             {{

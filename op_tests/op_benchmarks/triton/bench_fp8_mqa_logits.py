91c75e1dfc6e25ef64b149f9b0c9bf3b5bb2fd07
Mehmet Cagri Kaymak <mehmet.kaymak@amd.com>
2025-11-12 17:19:25 +0000

    formatting

--- op_tests/op_benchmarks/triton/bench_fp8_mqa_logits.py
+++ op_tests/op_benchmarks/triton/bench_fp8_mqa_logits.py
@@ -1,15 +1,18 @@
-
-
 import torch
 import triton
 import argparse
 from aiter.ops.triton.fp8_mqa_logits import fp8_mqa_logits
 from aiter.ops.triton.utils.types import e4m3_dtype
-from op_tests.triton_tests.test_fp8_mqa_logits import per_custom_dims_cast_to_fp8, generate_cp_test_data
+from op_tests.triton_tests.test_fp8_mqa_logits import (
+    per_custom_dims_cast_to_fp8,
+    generate_cp_test_data,
+)
 from op_tests.op_benchmarks.triton.utils.benchmark_utils import (
     print_vgpr,
     get_caller_name_no_ext,
 )
+
+
 def calculate_tflops(start_inds, end_inds, num_heads_q, head_dim, time_ms):
     time_s = time_ms * 1e-3
     start_inds = start_inds.to("cpu").numpy()
@@ -19,16 +22,13 @@ def calculate_tflops(start_inds, end_inds, num_heads_q, head_dim, time_ms):
         start = start_inds[i]
         end = end_inds[i]
 
-        total_flops += (
-            2.0 * num_heads_q * head_dim * (end - start)
-        )
-    total_flops = (
-        2.0 * num_heads_q * head_dim * 1024 * 1024 // 2
-    )
+        total_flops += 2.0 * num_heads_q * head_dim * (end - start)
+    total_flops = 2.0 * num_heads_q * head_dim * 1024 * 1024 // 2
     # TFLOPs = total FLOPs / (time in seconds * 1e12)
     tflops = total_flops / (time_s * 1e12)
 
     return tflops
+
 
 def run_benchmark(args):
     x_names = ["seq_q_l", "seq_kv_l", "num_heads_q", "head_dim"]
@@ -55,21 +55,24 @@ def run_benchmark(args):
     )
 
     @triton.testing.perf_report([benchmark])
-    def bench_fp8_mqa_logits(seq_q_l, seq_kv_l, num_heads_q, head_dim, metric, **kwargs):
-        q = torch.randn(seq_q_l, num_heads_q, head_dim, device='cuda', dtype=torch.bfloat16)
-        kv = torch.randn(seq_kv_l, head_dim, device='cuda', dtype=torch.bfloat16)
-        weights = torch.randn(seq_q_l, num_heads_q, device='cuda', dtype=torch.float32)
+    def bench_fp8_mqa_logits(
+        seq_q_l, seq_kv_l, num_heads_q, head_dim, metric, **kwargs
+    ):
+        q = torch.randn(
+            seq_q_l, num_heads_q, head_dim, device="cuda", dtype=torch.bfloat16
+        )
+        kv = torch.randn(seq_kv_l, head_dim, device="cuda", dtype=torch.bfloat16)
+        weights = torch.randn(seq_q_l, num_heads_q, device="cuda", dtype=torch.float32)
 
-
-        ks = torch.zeros(seq_q_l, dtype=torch.int, device='cuda')
-        ke = torch.arange(seq_q_l, dtype=torch.int, device='cuda') + (seq_kv_l - seq_q_l)
-
+        ks = torch.zeros(seq_q_l, dtype=torch.int, device="cuda")
+        ke = torch.arange(seq_q_l, dtype=torch.int, device="cuda") + (
+            seq_kv_l - seq_q_l
+        )
 
         q_fp8 = q.to(e4m3_dtype)
-        kv_fp8, scales = per_custom_dims_cast_to_fp8(kv, (0, ), False)
+        kv_fp8, scales = per_custom_dims_cast_to_fp8(kv, (0,), False)
 
         func = lambda: fp8_mqa_logits(q_fp8, kv_fp8, scales, weights, ks, ke)
-
 
         time_ms = triton.testing.do_bench(func, warmup=25, rep=100)
         tflops = calculate_tflops(ks, ke, num_heads_q, head_dim, time_ms)
@@ -84,12 +87,17 @@ def run_benchmark(args):
 
     bench_fp8_mqa_logits.run(save_path="." if args.o else None, print_data=True)
 
+
 def main():
     parser = argparse.ArgumentParser(description="FP8 MQA Logits Benchmark")
-    parser.add_argument('--num_heads_q', type=int, default=64, help='num. q heads')
-    parser.add_argument('--head_dim', type=int, default=128, help='head dim size')
-    parser.add_argument('--seq_q_l', type=int, default=4096, help='Input sequence length')
-    parser.add_argument('--seq_kv_l', type=int, default=4096, help='Output sequence length')
+    parser.add_argument("--num_heads_q", type=int, default=64, help="num. q heads")
+    parser.add_argument("--head_dim", type=int, default=128, help="head dim size")
+    parser.add_argument(
+        "--seq_q_l", type=int, default=4096, help="Input sequence length"
+    )
+    parser.add_argument(
+        "--seq_kv_l", type=int, default=4096, help="Output sequence length"
+    )
     parser.add_argument(
         "-o", action="store_true", help="Write performance results to CSV file"
     )
@@ -104,7 +112,5 @@ def main():
     run_benchmark(args)
 
 
-
 if __name__ == "__main__":
     main()
-

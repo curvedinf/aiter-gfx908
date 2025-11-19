2595e080ae3fa7e37ff856b019c509c3845bc30e
xytpai <xytpai@foxmail.com>
2025-11-19 06:10:21 +0000

    refine tests

--- op_tests/test_trtllm_all_reduce_fusion.py
+++ op_tests/test_trtllm_all_reduce_fusion.py
@@ -1,6 +1,5 @@
 import os
 import torch
-import torch.nn as nn
 import torch.multiprocessing as mp
 import torch.distributed as dist
 import aiter
@@ -13,111 +12,126 @@ for k, v in envs.items():
     os.environ[k] = v
 
 
-class RMSNorm(nn.Module):
-    def __init__(self, dim, norm_eps=1e-6, dtype=torch.float):
-        super().__init__()
-        self.eps = norm_eps
-        self.weight = nn.Parameter(torch.randn(dim, dtype=dtype), requires_grad=False)
-
-    def forward(self, x):
-        input_dtype = x.dtype
-        variance = x.float().pow(2).mean(-1, keepdim=True)
-        x = x * torch.rsqrt(variance + self.eps)
-        x = x.to(input_dtype)
-        return self.weight * x
+def rms_norm_forward(x: torch.Tensor, weight: torch.Tensor, eps: float):
+    input_dtype = x.dtype
+    variance = x.float().pow(2).mean(-1, keepdim=True)
+    x = x * torch.rsqrt(variance + eps)
+    x = x.to(input_dtype)
+    return weight * x
 
 
-def setup(rank, world_size):
-    torch.cuda.set_device(rank)
-    dist.init_process_group(
-        backend="nccl",
-        init_method="tcp://127.0.0.1:23459",
-        rank=rank,
-        world_size=world_size,
-    )
+class DistributedEnv:
+    def __init__(self, rank, world_size):
+        torch.cuda.set_device(rank)
+        dist.init_process_group(
+            backend="nccl",
+            init_method="tcp://127.0.0.1:23459",
+            rank=rank,
+            world_size=world_size,
+        )
+        self.rank = rank
+        self.world_size = world_size
+        self.group = dist.group.WORLD
+        self.trtllm_instance = aiter.TRTLLMAllreduceFusion(group=self.group)
+        self.barrier()
+
+    def __del__(self):
+        dist.destroy_process_group(self.group)
+
+    def barrier(self):
+        torch.cuda.set_device(self.rank)
+        dist.barrier(self.group)
+        torch.cuda.synchronize()
+
+    def allreduce_add_rms_native(self, allreduce_in, residual_in, rms_weight, eps):
+        dist.all_reduce(allreduce_in)
+        residual_out = allreduce_in + residual_in
+        norm_out = rms_norm_forward(residual_out, rms_weight, eps)
+        return residual_out, norm_out
+
+    def allreduce_add_rms_fused(self, allreduce_in, residual_in, rms_weight, eps):
+        residual_out = torch.empty_like(residual_in)
+        norm_out = torch.empty_like(allreduce_in)
+        aiter.trtllm_allreduce_rms(
+            self.rank,
+            self.world_size,
+            allreduce_in,
+            residual_in,
+            rms_weight,
+            residual_out,
+            norm_out,
+            eps,
+            False,
+            self.trtllm_instance.get_workspace(allreduce_in),
+        )
+        return residual_out, norm_out
 
 
 def worker(
-    rank,
-    world_size,
-    allreduce_in,
-    residual_in,
-    rms,
-    ref_residual_out,
-    ref_norm_out,
-    eps,
-    use_fused=True,
+    rank, world_size, allreduce_in_, residual_in_, rms_weight_, eps, show_profile=False
 ):
-    setup(rank, world_size)
-    trtllm_instance = aiter.TRTLLMAllreduceFusion()
-    num_tokens, hidden_dim = residual_in.shape
-    local_allreduce_in = allreduce_in[rank].cuda(rank)
-    local_residual_in = residual_in.cuda(rank)
-    local_rms = rms.cuda(rank)
-    prof = torch.profiler.profile(
-        activities=[
-            torch.profiler.ProfilerActivity.CPU,
-            torch.profiler.ProfilerActivity.CUDA,
-        ]
-    )
-    with prof:
-        if not use_fused:
-            dist.all_reduce(local_allreduce_in)
-            local_norm_out = local_rms(local_allreduce_in + local_residual_in)
-        else:
-            local_residual_out = torch.empty_like(local_residual_in)
-            local_norm_out = torch.empty_like(local_allreduce_in)
-            aiter.trtllm_allreduce_rms(
-                rank,
-                world_size,
-                local_allreduce_in,
-                local_residual_in,
-                local_rms.weight.data,
-                local_residual_out,
-                local_norm_out,
-                eps,
-                False,
-                trtllm_instance.get_workspace(local_allreduce_in),
+    dist_env = DistributedEnv(rank, world_size)
+    for i in range(len(allreduce_in_)):
+        local_allreduce_in = allreduce_in_[i][rank].cuda(rank)
+        local_residual_in = residual_in_[i].cuda(rank)
+        local_rms_weight = rms_weight_[i].cuda(rank)
+        num_tokens, hidden_dim = local_allreduce_in.shape
+        prof = torch.profiler.profile(
+            activities=[
+                torch.profiler.ProfilerActivity.CPU,
+                torch.profiler.ProfilerActivity.CUDA,
+            ]
+        )
+        with prof:
+            ref_residual_out, ref_norm_out = dist_env.allreduce_add_rms_native(
+                local_allreduce_in.clone(), local_residual_in, local_rms_weight, eps
             )
-    maxdiff = (local_norm_out.cpu() - ref_norm_out).abs().max()
-    print(f"rank:{rank}, maxdiff:{maxdiff}")
-    # assert torch.allclose(local_norm_out.cpu(), ref_norm_out)
-    if rank == 0:
-        print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10000))
-    dist.destroy_process_group()
+            residual_out, norm_out = dist_env.allreduce_add_rms_fused(
+                local_allreduce_in.clone(), local_residual_in, local_rms_weight, eps
+            )
+        if rank == 0 and show_profile:
+            print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10000))
+        maxdiff = (norm_out.cpu().float() - ref_norm_out.cpu().float()).abs().max()
+        print(f"rank:{rank}, maxdiff:{maxdiff}")
+
+
+def testcase(
+    world_size=4,
+    num_tokens=128,
+    hidden_dim=1024,
+    eps=1e-6,
+    dtype=torch.float,
+    nsamples=5,
+):
+    allreduce_in_ = []
+    residual_in_ = []
+    rms_weight_ = []
+    for i in range(nsamples):
+        allreduce_in_.append(
+            torch.randn(world_size, num_tokens, hidden_dim, dtype=dtype)
+        )
+        residual_in_.append(torch.randn(num_tokens, hidden_dim, dtype=dtype))
+        rms_weight_.append(torch.randn(hidden_dim, dtype=dtype))
+    mp.spawn(
+        worker,
+        args=(
+            world_size,
+            allreduce_in_,
+            residual_in_,
+            rms_weight_,
+            eps,
+        ),
+        nprocs=world_size,
+        join=True,
+    )
 
 
 def main():
-    def testcase(
-        world_size=4, num_tokens=128, hidden_dim=1024, eps=1e-6, dtype=torch.float
-    ):
-        allreduce_in = torch.randn(world_size, num_tokens, hidden_dim, dtype=dtype)
-        residual_in = torch.randn(num_tokens, hidden_dim, dtype=dtype)
-        rms = RMSNorm(hidden_dim, dtype=dtype)
-        ref_residual_out = allreduce_in.sum(dim=0) + residual_in
-        ref_norm_out = rms(ref_residual_out)
-        mp.spawn(
-            worker,
-            args=(
-                world_size,
-                allreduce_in,
-                residual_in,
-                rms,
-                ref_residual_out,
-                ref_norm_out,
-                eps,
-            ),
-            nprocs=world_size,
-            join=True,
-        )
-
     num_tokens = 129
-    testcase(num_tokens=num_tokens, dtype=torch.float)
     testcase(num_tokens=num_tokens, dtype=torch.float)
     testcase(num_tokens=num_tokens, dtype=torch.bfloat16)
     testcase(num_tokens=num_tokens, dtype=torch.half)
     num_tokens = 128
-    testcase(num_tokens=num_tokens, dtype=torch.float)
     testcase(num_tokens=num_tokens, dtype=torch.float)
     testcase(num_tokens=num_tokens, dtype=torch.bfloat16)
     testcase(num_tokens=num_tokens, dtype=torch.half)

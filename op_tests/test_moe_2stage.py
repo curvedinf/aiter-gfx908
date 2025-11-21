e3a9d843a9848d85fe5cce15f0f7fa9d567301b5
yadaish <yadai@amd.com>
2025-11-21 07:21:01 +0000

    update

--- op_tests/test_moe_2stage.py
+++ op_tests/test_moe_2stage.py
@@ -283,9 +283,10 @@ def test_fmoe(
         hidden_pad=hidden_pad,
         bias1=exp_bias1_aiter,
         bias2=exp_bias2_aiter,
-        num_iters=1,
-        num_warmup=0,
+        num_iters=3,
+        num_warmup=1,
     )
+    aiter.logger.info(f'run pertest done')
     err = checkAllclose(
         out2_ref,
         out2_ck,

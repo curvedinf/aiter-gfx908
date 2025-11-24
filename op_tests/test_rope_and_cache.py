b70eabf724fffc0d20efad683d1e44bc26da040a
amd-ruitang3 <Rui.Tang2@amd.com>
2025-11-24 04:02:49 +0000

    merge sep & fuse test

--- op_tests/test_rope_and_cache.py
+++ op_tests/test_rope_and_cache.py
@@ -216,7 +216,6 @@ def test_reshape_and_cache(
     asm_layout = True
     kvhead = num_heads
     num_blocks = (MAX_TOKEN_SUPPORTED + block_size - 1) // block_size
-    # num_blocks = (ctx_lens+1+block_size-1)//block_size
     max_token_num_support = num_blocks * block_size
     x = 16 // DType_KVCache.itemsize
     if asm_layout:
@@ -228,10 +227,6 @@ def test_reshape_and_cache(
         v_cache_shape = (bs * num_blocks, kvhead, head_size, block_size)
         kv_scale_shape = (kvhead, bs * max_token_num_support)
 
-    # ##################################################### prefill part
-    # value = torch.randn(
-    #     bs * ctx_lens, kvhead, head_size, dtype=DType_KV, device="cuda"
-    # )
     device = key.device
     k_cache = torch.empty(k_cache_shape, dtype=DType_KVCache, device=device)
     v_cache = torch.empty(v_cache_shape, dtype=DType_KVCache, device=device)
@@ -247,20 +242,6 @@ def test_reshape_and_cache(
             for i in range(ctx_lens)
         ]
     ).cuda()
-
-    # k_cache_ref = k_cache.clone()
-    # v_cache_ref = v_cache.clone()
-    # out_ref, us_ref = run_torch(
-    #     key.view(bs, ctx_lens, kvhead, head_size),
-    #     value.view(bs, ctx_lens, kvhead, head_size),
-    #     k_cache_ref,
-    #     v_cache_ref,
-    #     slot_mapping,
-    #     block_size,
-    #     x,
-    #     asm_layout,
-    #     quantCfg,
-    # )
 
     k_cache_a = k_cache.clone()
     v_cache_a = v_cache.clone()
@@ -280,79 +261,6 @@ def test_reshape_and_cache(
     )
     ret["us_prefill"] = us_a
 
-    # print(f"prefill part: ref vs aiter {us_ref:>8.2f}us vs {us_a:>8.2f}us")
-    # names = ["k_cache", "v_cache", "k_scale", "v_scale"]
-    # for i, el in enumerate(out_ref):
-    #     if el is None:
-    #         continue
-    #     checkAllclose(
-    #         el.to(dtypes.fp32),
-    #         out_a[i].to(dtypes.fp32),
-    #         msg=f"{names[i]} {el.shape}",
-    #     )
-
-    # ##################################################### decode part
-    
-    # Decode part here is not compatiable with test, deprecate it currently
-
-    # qkv = torch.randn(bs, qhead + 2 * kvhead, head_size, dtype=DType_KV, device="cuda")
-    # _, key, value = torch.split(qkv, [qhead, kvhead, kvhead], dim=1)
-    # value = torch.randn(
-    #     bs, kvhead, head_size, dtype=DType_KV, device="cuda"
-    # )
-
-    # if quantCfg:
-    #     quantCfg["k_scale"] = k_scale.clone()
-    #     quantCfg["v_scale"] = v_scale.clone()
-    # slot_mapping = torch.tensor(
-    #     [bsID * max_token_num_support + ctx_lens for bsID in range(bs)]
-    # ).cuda()
-
-    # k_cache_ref = k_cache.clone()
-    # v_cache_ref = v_cache.clone()
-    # out_ref, us_ref = run_torch(
-    #     key.view(bs, 1, kvhead, head_size),
-    #     value.view(bs, 1, kvhead, head_size),
-    #     k_cache_ref,
-    #     v_cache_ref,
-    #     slot_mapping,
-    #     block_size,
-    #     x,
-    #     asm_layout,
-    #     quantCfg,
-    # )
-
-    # k_cache_a = k_cache.clone()
-    # v_cache_a = v_cache.clone()
-    # if quantCfg:
-    #     quantCfg["k_scale"] = k_scale.clone()
-    #     quantCfg["v_scale"] = v_scale.clone()
-    # out_a, us_a = run_aiter(
-    #     key,
-    #     value,
-    #     k_cache_a,
-    #     v_cache_a,
-    #     slot_mapping,
-    #     block_size,
-    #     x,
-    #     asm_layout,
-    #     quantCfg,
-    # )
-    # ret["us_decode"] = us_a
-
-    # print(f"decode part: ref vs aiter {us_ref:>8.2f}us vs {us_a:>8.2f}us")
-    # names = ["k_cache", "v_cache", "k_scale", "v_scale"]
-    # for i, el in enumerate(out_ref):
-    #     if el is None:
-    #         continue
-    #     checkAllclose(
-    #         el.to(dtypes.fp32),
-    #         out_a[i].to(dtypes.fp32),
-    #         msg=f"{names[i]} {el.shape}",
-    #     )
-    # print(
-    #     f"finish test {ctx_lens=} {bs=} {num_heads=} {head_size=} {block_size=} {DType_KV=} {DType_KVCache=}"
-    # )
     return ret
 
 @perftest()

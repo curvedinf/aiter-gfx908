fd4f3bdda64e7f1ba8adf68fac02f466b6799b3f
yadaish <yadai@amd.com>
2025-12-04 17:23:44 +0000

    update

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -222,8 +222,9 @@ def fused_moe_(
     ], f"Fused_moe unsupported out dtype: {dtype}"
     quant_type = quant_remap.get(quant_type, quant_type)
     q_dtype_w = w1.dtype
-    q_dtype_a = w1.dtype if w1.dtype != torch.uint32 else dtypes.fp8
+    # q_dtype_a = w1.dtype if w1.dtype != torch.uint32 else dtypes.fp8
     # q_dtype_a = dtypes.fp4x2 if quant_type == QuantType.per_1x32 else q_dtype_a
+    q_dtype_a = hidden_states.dtype
 
     metadata = get_2stage_cfgs(
         get_padded_M(M),  # consider token_num > 1024 as prefill

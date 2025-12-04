ec23f6b803d92c52e3d24eb195baaf89ac407efe
ZhangLirong-amd <Lirong.Zhang@amd.com>
2025-12-04 09:58:25 +0800

    use hip reduce for test

--- csrc/kernels/mla/reduce.cu
+++ csrc/kernels/mla/reduce.cu
@@ -506,6 +506,14 @@ __global__ void kn_mla_reduce_v1(
     MLA_MERGE_CASE_EF(                                                                                      \
         NUM_HEAD,  16, HEAD_DIM, 128, OUTPUT_LSE, false, NRFM, false, NAME, __VA_ARGS__)                    \
     MLA_MERGE_CASE_EF(                                                                                      \
+        NUM_HEAD,  1, HEAD_DIM, 128, OUTPUT_LSE, true,  NRFM, true,  NAME, __VA_ARGS__)                    \
+    MLA_MERGE_CASE_EF(                                                                                      \
+        NUM_HEAD,  1, HEAD_DIM, 128, OUTPUT_LSE, true,  NRFM, false, NAME, __VA_ARGS__)                    \
+    MLA_MERGE_CASE_EF(                                                                                      \
+        NUM_HEAD,  1, HEAD_DIM, 128, OUTPUT_LSE, false, NRFM, true,  NAME, __VA_ARGS__)                    \
+    MLA_MERGE_CASE_EF(                                                                                      \
+        NUM_HEAD,  1, HEAD_DIM, 128, OUTPUT_LSE, false, NRFM, false, NAME, __VA_ARGS__)                    \    
+    MLA_MERGE_CASE_EF(                                                                                      \
         NUM_HEAD,  16, HEAD_DIM, 512, OUTPUT_LSE, true,  NRFM, true,  NAME, __VA_ARGS__)                    \
     MLA_MERGE_CASE_EF(                                                                                      \
         NUM_HEAD,  16, HEAD_DIM, 512, OUTPUT_LSE, true,  NRFM, false, NAME, __VA_ARGS__)                    \

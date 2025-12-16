ed5efa543df2b0eebd89ff45036639a3c2f06095
Shucai Xiao <shucai.xiao@amd.com>
2025-12-16 14:42:51 -0600

    refine the unit test

--- op_tests/test_rmsnorm_quantize_fuse.py
+++ op_tests/test_rmsnorm_quantize_fuse.py
@@ -1,20 +1,19 @@
-from aiter import (
-    rmsnorm2d_fwd,
-    rmsnorm2d_fwd_with_add,
-    rms_norm,
-    layernorm2d_fwd,
-    layernorm2d_fwd_with_add,
+import torch
+import pytest 
+from aiter.test_common import (
+    checkAllclose,
 )
 
-from aiter.ops.quant import per_tensor_quant_hip, per_tensor_quant_triton
-import torch
+from aiter import (
+    rmsnorm2d_fwd,
+)
+
+from aiter.ops.quant import per_tensor_quant_hip
 
 from aiter.ops.triton.fused_fp8_quant import fused_rms_fp8_per_tensor_static_quant
 import aiter as rocm_aiter
-from aiter.ops.triton.rmsnorm import _rmsnorm_forward_with_add, _rmsnorm_forward
 
 rocm_aiter_fp8_dtype = rocm_aiter.dtypes.fp8
-print(f"fp8_type = {rocm_aiter_fp8_dtype}")
 
 def rmsnorm2d_fwd_(
     x: torch.Tensor, weight: torch.Tensor, eps: float, dim: int
@@ -23,41 +22,52 @@ def rmsnorm2d_fwd_(
     x = x.reshape(-1, dim)
     return rmsnorm2d_fwd(x, weight, eps).view(ori_shape)
 
-torch.manual_seed(1)
-hidden_size = 8192
-x_shape = (2, hidden_size)
-dtype = torch.bfloat16
-x = torch.randn(x_shape, dtype=dtype, device='cuda')
-w = torch.ones(hidden_size, dtype=dtype).cuda()
-eps = 0.1
 
-DTYPE_MAX = 448.0
-tmp_rms_out = rmsnorm2d_fwd_(x, w, eps, hidden_size)
-
-abs_x = torch.abs(tmp_rms_out)
-x_max = torch.max(abs_x)
-scale_val = x_max / DTYPE_MAX
-
-x_scale = torch.tensor((scale_val), dtype=torch.float32, device='cuda')
-
-def atom_rmsnorm_quantize():
-    rms_out = rmsnorm2d_fwd_(x, w, eps, hidden_size)
-    quant_out, scale = per_tensor_quant_hip(rms_out, x_scale, rocm_aiter_fp8_dtype)
+def rmsnorm_fp8_quantization_ref(x, w, x_scale, eps, n, rocm_fp8_dtype):
+    rms_out = rmsnorm2d_fwd_(x, w, eps, n)
+    quant_out, _ = per_tensor_quant_hip(rms_out, x_scale, rocm_fp8_dtype)
     return quant_out, rms_out
 
 
-def atom_rmsnorm_quant_fusion():
+def triton_rmsnorm_fp8_quantization_fuse(x, w, x_scale, eps, rocm_fp8_dtype):
     quant_out, rms_out, _, _ = fused_rms_fp8_per_tensor_static_quant(x, w, eps, x_scale, None, None, eps, 
-                                                                     dtype_quant=rocm_aiter_fp8_dtype, 
+                                                                     dtype_quant=rocm_fp8_dtype, 
                                                                      res1=None,
                                                                      output_unquantized_inp1=True,
                                                                      intermediate_convert_to_inp1_type=True)
     return quant_out, rms_out
 
 
-def test_atom_fusion():
-    x_nonfusion_out, rms_out_nonfusion = atom_rmsnorm_quantize()
-    x_fusion_out, rms_out_fusion = atom_rmsnorm_quant_fusion()
+@pytest.mark.parametrize("m, n", [
+    (m, n) 
+    for m in [1, 2, 4, 8, 256, 1024, 8192]
+    for n in [128, 4096, 8192]
+])
+def test_rms_quantization_fuse(m, n):
+    eps = 0.0001
+    rocm_fp8_dtype = rocm_aiter_fp8_dtype
+    dim = (m, n)
 
-    torch.testing.assert_close(rms_out_fusion, rms_out_nonfusion)
-    torch.testing.assert_close(x_fusion_out.to(torch.float32), x_nonfusion_out.to(torch.float32))
+    x_shape = (m, n)
+    dtype = torch.bfloat16
+    x = torch.randn(x_shape, dtype=dtype, device='cuda')
+    w = torch.ones(n, dtype=dtype).cuda()
+
+    DTYPE_MAX = (
+        torch.finfo(rocm_fp8_dtype).max
+        if torch.is_floating_point(x)
+        else torch.iinfo(rocm_fp8_dtype).max
+    )
+
+    # calculate the correct scale value
+    rms_out = rmsnorm2d_fwd_(x, w, eps, n)
+    rms_out_abs = torch.abs(rms_out)
+    rms_out_abs_max = torch.max(rms_out_abs)
+    scale_val = rms_out_abs_max / DTYPE_MAX
+    x_scale = torch.tensor((scale_val), dtype=torch.float32, device='cuda')
+
+    fp8_x_ref, rms_out_ref = rmsnorm_fp8_quantization_ref(x, w, x_scale, eps, n, rocm_fp8_dtype)
+    fp8_x, rms_out = triton_rmsnorm_fp8_quantization_fuse(x, w, x_scale, eps, rocm_fp8_dtype)
+
+    checkAllclose(rms_out, rms_out_ref)
+    checkAllclose(fp8_x.to(torch.float32), fp8_x_ref.to(torch.float32))

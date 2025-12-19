32b1f6b50f7ba17eebf220559914b23cdf23e157
Tianxing <chi0chu319@gmail.com>
2025-12-19 12:22:07 +0000

    quant kenrel fixed

--- op_tests/op_benchmarks/triton/bench_diffusion_attention.py
+++ op_tests/op_benchmarks/triton/bench_diffusion_attention.py
@@ -40,7 +40,6 @@ from aiter.ops.triton.fav3_sage import (
 from aiter.ops.triton._triton_kernels.sage_attn_triton_amd import (
     get_fwd_configs,
     sage_quant,
-    quantize_v_fp8
 )
 
 
@@ -243,32 +242,19 @@ def fav3_sage_forward_func(
     fp8_dtype = aiter.dtypes.fp8
     FP8_MAX = torch.finfo(fp8_dtype).max
 
-    q_int8, q_descale, k_int8, k_descale, _ = per_block_int8(
+    # Method 2: Current sage_quant (unified quantization)
+    q_int8, q_descale, k_int8, k_descale, v_fp8, v_descale, _ = sage_quant(
         q,
         k,
+        v,
+        fp8_dtype,
+        FP8_MAX,
         km=k_mean,
         sm_scale=softmax_scale,
         BLKQ=BLKQ,
         BLKK=BLKK,
         tensor_layout="NHD",
     )
-    v_fp8, v_descale = quantize_v_fp8(v, FP8_MAX, BLKK=BLKK, tensor_layout="NHD")
-
-
-    # Method 2: Current sage_quant (unified quantization)
-    # q_int8, q_descale, k_int8, k_descale, v_fp8, v_descale, _ = sage_quant(
-    #     q,
-    #     k,
-    #     v,
-    #     fp8_dtype,
-    #     FP8_MAX,
-    #     km=k_mean,
-    #     sm_scale=softmax_scale,
-    #     BLKQ=BLKQ,
-    #     BLKK=BLKK,
-    #     tensor_layout="NHD",
-    # )
-
     return lambda: fav3_sage_func(
         q_int8,
         k_int8,

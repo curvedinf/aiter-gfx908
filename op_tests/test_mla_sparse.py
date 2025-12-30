a8d7111ea9d97bc709fbb54f9ef5278400bdcf8c
zanzhang <zanzhang@amd.com>
2025-12-30 08:30:05 +0800

    revert triton kernel

--- op_tests/test_mla_sparse.py
+++ op_tests/test_mla_sparse.py
@@ -251,65 +251,6 @@ def _convert_req_index_to_global_index_kernel(
     tl.store(out_ptr_ij, out_val)
 
 
-@triton.jit
-def _convert_req_index_to_global_index_kernel(
-    kv_indptr,  # int32 [num_requests]
-    kv_indices,  # int32 [num_requests * max_num_blocks_per_req]
-    token_indices_ptr,  # int32 [num_tokens, NUM_TOPK_TOKENS]
-    out_ptr,  # int32 [num_tokens, NUM_TOPK_TOKENS]
-    # shapes (compile-time where possible)
-    BLOCK_SIZE: tl.constexpr,
-    BLOCK_N: tl.constexpr,  # tile width along columns
-    # strides (in elements)
-    bt_stride0: tl.constexpr,
-    ti_stride0: tl.constexpr,
-    ti_stride1: tl.constexpr,
-    out_stride0: tl.constexpr,
-    out_stride1: tl.constexpr,
-    qo_len: tl.constexpr,
-):
-    # program_id(0) -> token_id (row)
-    # program_id(1) -> tile index along columns
-    token_id = tl.program_id(0)
-    tile_id = tl.program_id(1)
-
-    # Each program covers BLOCK_N consecutive columns
-    indice_id = tile_id * BLOCK_N + tl.arange(0, BLOCK_N)
-    batch_id = token_id // qo_len
-
-    # Load request id for this token (no mask: grid is exact)
-    kv_start = tl.load(kv_indptr + batch_id)
-    kv_end = tl.load(kv_indptr + batch_id + 1)
-    kv_len = kv_end - kv_start
-
-    # Load token indices for this tile
-    ti_ptr = token_indices_ptr + token_id * ti_stride0 + indice_id * ti_stride1
-    tok = tl.load(ti_ptr)  # int32
-
-    # Only token == -1 should propagate as -1
-    is_invalid_tok = tok < 0
-
-    # Compute block id and in-block offset
-    block_id = tok // BLOCK_SIZE
-    inblock_off = tok % BLOCK_SIZE
-
-    # Guard block_table access
-    valid_block = indice_id < kv_len
-    # tl.device_print("offset", valid_block)
-    base = tl.load(
-        kv_indices + kv_start + block_id * bt_stride0, mask=valid_block, other=0
-    )
-
-    # If token == -1 OR block_id OOB, output -1; else base * BLOCK_SIZE + offset
-    out_val = tl.where(
-        is_invalid_tok | (~valid_block), -1, base * BLOCK_SIZE + inblock_off
-    )
-
-    # Store results
-    out_ptr_ij = out_ptr + token_id * out_stride0 + indice_id * out_stride1
-    tl.store(out_ptr_ij, out_val)
-
-
 def triton_convert_req_index_to_global_index(
     kv_indptr: torch.Tensor,  # int32 [num_tokens + 1]
     kv_indices: torch.Tensor,  # int32 [total_kv_seqlen]

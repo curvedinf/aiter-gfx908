5331699953d6a853a9fbc3fdfd0a4e36ab975cdc
lalala-sh <Jiaxing.Wen@amd.com>
2026-01-05 21:28:44 +0800

    moe tunner fix (#1743)
    * support block_m=16
    
    * update
    
    * update tuned fmoe csv
    
    * update
    
    * update
    
    * update fmoe tuned csv
    
    * fix typo
    
    * fix tunner when use asm_stage1 and ck_stage2
    
    * code style
    
    * fix condition
    
    * fix default dispatch when stage2 k = 128
    
    ---------
    
    Co-authored-by: lalala-sh <root@hjbog-srdc-39.amd.com>

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2024-2026, Advanced Micro Devices, Inc. All rights reserved.
 
 import functools
 import os
@@ -815,17 +815,20 @@ def get_2stage_cfgs(
             False,
             True,
         )
-    if (
-        "ck2stages" in kernelName1
-        or (q_type == QuantType.per_1x128 and doweight_stage1)
-        or q_dtype_w
-        in [
-            dtypes.bf16,
-            dtypes.fp16,
-            torch.uint32,
-            dtypes.fp4x2,
-            dtypes.fp8,
-        ]
+
+    if (kernelName1 and "ck2stages" in kernelName1) or (
+        not kernelName1
+        and (
+            (q_type == QuantType.per_1x128 and doweight_stage1)
+            or q_dtype_w
+            in [
+                dtypes.bf16,
+                dtypes.fp16,
+                torch.uint32,
+                dtypes.fp4x2,
+                dtypes.fp8,
+            ]
+        )
     ):
         return MOEMetadata(
             functools.partial(
@@ -833,6 +836,7 @@ def get_2stage_cfgs(
                 kernelName=kernelName1,
                 activation=activation,
                 quant_type=q_type,
+                dtype=dtype,
                 splitk=ksplit,
             ),
             functools.partial(
@@ -847,7 +851,7 @@ def get_2stage_cfgs(
         )
 
     # TODO: remove when stage2 support more size
-    tmpList = [32, 64, 128]
+    tmpList = [16, 32, 64, 128]
     if block_m not in tmpList:
         tag = ""
         block_m = ([el for el in tmpList if block_m < el] + [128])[0]
@@ -1019,7 +1023,6 @@ def fused_moe_2stages(
             w1_scale.view(dtypes.fp8_e8m0) if w1.dtype == dtypes.fp4x2 else w1_scale
         ),
         sorted_weights=sorted_weights if doweight_stage1 else None,
-        dtype=dtype,
         **extra_stage1_args,
     )
 

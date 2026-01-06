47a0f909f352d48e059fb99c60aa72eddc30fb4b
Yanxing-Shi <yanxishi@amd.com>
2026-01-06 10:02:00 +0000

    replace instance

--- csrc/ck_gemm_a8w8_blockscale/gemm_a8w8_blockscale_instance_tile.py
+++ csrc/ck_gemm_a8w8_blockscale/gemm_a8w8_blockscale_instance_tile.py
@@ -69,37 +69,29 @@ class TileKernelInstance:
 candidate_kernels_dict_tile = {
     #######################| M_Tile | N_Tile | K_Tile | M_Warp | N_Warp | K_Warp | M_Warp_Tile | N_Warp_Tile | K_Warp_Tile |   Scheduler   | TiledMMAPermuteN |  TransposeC | DoubleSmemBuffer | UsePersistentKernel | BlockPerCu |
     # K_Tile = 128, M_Warp x N_Warp = 1 x 4, WarpTile = 16 x 16 x 32
-    0:   TileKernelInstance(   16,     128,      128,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    1:   TileKernelInstance(   32,     128,      128,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    2:   TileKernelInstance(   64,     128,      128,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    3:   TileKernelInstance(  128,     128,      128,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    # K_Tile = 128, M_Warp x N_Warp = 2 x 2, WarpTile = 16 x 16 x 32
-    4:   TileKernelInstance(   32,     128,      128,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    5:   TileKernelInstance(   64,     128,      128,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    6:   TileKernelInstance(  128,     128,      128,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    # K_Tile = 128, M_Warp x N_Warp = 1 x 4, WarpTile = 32 x 32 x 16
-    7:   TileKernelInstance(   32,     128,      128,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
-    8:   TileKernelInstance(   64,     128,      128,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
-    9:   TileKernelInstance(  128,     128,      128,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
-    # K_Tile = 128, M_Warp x N_Warp = 2 x 2, WarpTile = 32 x 32 x 16
-    10:  TileKernelInstance(   64,     128,      128,     2,        2,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
-    11:  TileKernelInstance(  128,     128,      128,     2,        2,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
+    0:   TileKernelInstance(   32,     128,      128,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    1:   TileKernelInstance(   64,     128,      128,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    2:   TileKernelInstance(  128,     128,      128,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
     # K_Tile = 256, M_Warp x N_Warp = 1 x 4, WarpTile = 16 x 16 x 32
-    12:  TileKernelInstance(   16,     128,      256,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    13:  TileKernelInstance(   32,     128,      256,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    14:  TileKernelInstance(   64,     128,      256,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    15:  TileKernelInstance(  128,     128,      256,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    3:   TileKernelInstance(   32,     128,      256,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    4:   TileKernelInstance(   64,     128,      256,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    5:   TileKernelInstance(  128,     128,      256,     1,        4,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    # K_Tile = 128, M_Warp x N_Warp = 2 x 2, WarpTile = 16 x 16 x 32  
+    6:   TileKernelInstance(   64,     128,      128,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    7:   TileKernelInstance(  128,     128,      128,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
     # K_Tile = 256, M_Warp x N_Warp = 2 x 2, WarpTile = 16 x 16 x 32
-    16:  TileKernelInstance(   32,     128,      256,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    17:  TileKernelInstance(   64,     128,      256,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
-    18:  TileKernelInstance(  128,     128,      256,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    8:   TileKernelInstance(   64,     128,      256,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    9:   TileKernelInstance(  128,     128,      256,     2,        2,       1,        16,            16,           32,      "Intrawave",        False,             False,        True,               False,             1      ),
+    # K_Tile = 128, M_Warp x N_Warp = 1 x 4, WarpTile = 32 x 32 x 16
+    10:  TileKernelInstance(   64,     128,      128,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
+    11:  TileKernelInstance(  128,     128,      128,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
     # K_Tile = 256, M_Warp x N_Warp = 1 x 4, WarpTile = 32 x 32 x 16
-    19:  TileKernelInstance(   32,     128,      256,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
-    20:  TileKernelInstance(   64,     128,      256,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
-    21:  TileKernelInstance(  128,     128,      256,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
+    12:  TileKernelInstance(   64,     128,      256,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
+    13:  TileKernelInstance(  128,     128,      256,     1,        4,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
+    # K_Tile = 128, M_Warp x N_Warp = 2 x 2, WarpTile = 32 x 32 x 16
+    14:  TileKernelInstance(  128,     128,      128,     2,        2,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
     # K_Tile = 256, M_Warp x N_Warp = 2 x 2, WarpTile = 32 x 32 x 16
-    22:  TileKernelInstance(   64,     128,      256,     2,        2,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
-    23:  TileKernelInstance(  128,     128,      256,     2,        2,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
+    15:  TileKernelInstance(  128,     128,      256,     2,        2,       1,        32,            32,           16,      "Intrawave",        False,             False,        True,               False,             1      ),
 }
 
 

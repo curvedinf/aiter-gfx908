f45c68051b4af16c805322482b8127fb57440b31
Juuso Korhonen <40278371+juuso-oskari@users.noreply.github.com>
2026-01-15 12:19:03 +0000

    fix

--- aiter/ops/triton/_triton_kernels/sage_attn_triton_amd/__init__.py
+++ aiter/ops/triton/_triton_kernels/sage_attn_triton_amd/__init__.py
@@ -1,4 +1,4 @@
 from . import interface_v1 as fav3_sage
-from .fwd_prefill import get_fwd_configs, sage_quant, V_QUANT_SCHEME
+from .fwd_prefill import get_fwd_configs, sage_quant
 
-__all__ = ["fav3_sage", "get_fwd_configs"]
+__all__ = ["fav3_sage", "get_fwd_configs", "sage_quant"]

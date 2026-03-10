4aebba3bc3c12dde7e9fc6bf7ef3b8a09d63df37
Aliasger Zaidy <aliasger.zaidy@amd.com>
2026-03-10 16:42:34 +0000

    Tuned basic GEMMs for latest Triton

--- aiter/ops/triton/_triton_kernels/gemm/basic/gemm_a8wfp4.py
+++ aiter/ops/triton/_triton_kernels/gemm/basic/gemm_a8wfp4.py
@@ -197,7 +197,7 @@ def _gemm_a8wfp4_kernel(
                     b_mask = offs_bk[:, None] < K - k * (BLOCK_SIZE_K // 2)
                     b = tl.load(b_ptrs, mask=b_mask, other=0)
                 b_scales = tl.load(b_scale_ptrs)
-            accumulator += tl.dot_scaled(a, a_ones_scale, "e4m3", b, b_scales, "e2m1")
+            accumulator += tl.dot_scaled(tl.cast(a, tl.uint8, bitcast=True), a_ones_scale, "e4m3", b, b_scales, "e2m1")
 
         # Scale by a_scales at the end
         c = (accumulator * a_scales[:, None]).to(c_ptr.type.element_ty)

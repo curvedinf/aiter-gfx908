60ea06ab59ab0a5ee2ac310d3c2ada5e32a07831
Farel Lukas <farlukas@amd.com>
2026-03-27 18:30:49 +0000

    Added kernel metadata for benchmarks to work

--- aiter/ops/triton/_gluon_kernels/moe/moe_op_gemm_a4w4.py
+++ aiter/ops/triton/_gluon_kernels/moe/moe_op_gemm_a4w4.py
@@ -1,5 +1,65 @@
+import torch
 from triton.experimental import gluon
 import triton.experimental.gluon.language as gl
+
+
+def matmul_launch_metadata(grid, kernel, args):
+    ret = dict()
+    M, N, K = None, args["N"], args["K"]
+    Y, X, W = args["Y"], args["X"], args["W"]
+    hist = args["ExptHist"]
+    if hist is not None:
+        n_rows = int(hist.float().mean())
+        n_tokens = float(hist.sum())
+        n_w_bytes = (W.numel() * W.element_size() // hist.numel()) * (hist > 0).sum()
+    else:
+        n_tokens = None
+        n_w_bytes = W.numel() * W.element_size()
+
+    def repr(s, x):
+        return f"{s}={x}" if x is not None else f"E_{len(hist)}({s})={n_rows}"
+
+    nbits = X.dtype.itemsize * 8
+    ret["name"] = f"{kernel.name} [{repr('M', M)}, {repr('N', N)}, {repr('K', K)}]"
+    gindx = args.get("GatherIndx", None)
+    # sindx = args.get("WriteBackIndx", None)
+    if gindx is not None:
+        ret["name"] += "_layer1"
+    else:
+        ret["name"] += "_layer2"
+    if args["B"] is not None:
+        ret["name"] += "_bias"
+    if args["APPLY_SWIGLU"]:
+        ret["name"] += "_swiglu"
+    if args["Quant_static_scale"] is not None:
+        ret["name"] += "_quant"
+
+    fM = n_tokens
+    fK = K if K is not None else n_tokens
+    ret[f"flops{nbits}"] = 2.0 * fM * N * fK
+
+    gindx = args.get("GatherIndx", None)
+    # sindx = args.get("WriteBackIndx", None)
+    n_x_bytes = X.numel() * X.element_size()
+    n_y_bytes = Y.numel() * Y.element_size()
+    if hist is not None:
+        assert n_tokens is not None
+        n_expts_act = args["N_EXPTS_ACT"]
+
+        if gindx is not None:
+            # recreate inverse GatherIndx.
+            dst = torch.full_like(gindx, -1)
+            idx = torch.arange(len(gindx), device=gindx.device, dtype=torch.int32)
+            mask = gindx != -1
+            dst[gindx[mask]] = idx[mask]
+            n_read_rows = (dst.view((-1, n_expts_act)) != -1).any(dim=1).sum()
+        else:
+            n_read_rows = n_tokens
+        n_x_bytes = n_read_rows * X.shape[-1] * X.element_size()
+        n_y_bytes = n_tokens * Y.shape[-1] * Y.element_size()
+    ret["bytes"] = int(n_x_bytes + n_y_bytes + n_w_bytes)
+
+    return ret
 
 
 @gluon.jit
@@ -165,7 +225,7 @@ def _reduce_grouped_gfx1250(
         gl.store(out_ptr, acc, mask=out_n_mask)
 
 
-@gluon.jit
+@gluon.jit(launch_metadata=matmul_launch_metadata)
 def _moe_gemm_a4w4_gfx1250(
     Y,
     stride_y_k,

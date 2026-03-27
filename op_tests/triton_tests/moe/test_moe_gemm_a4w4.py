2d4f0ac33e338f6de1b1c150c58b34e2e353864d
Farel Lukas <farlukas@amd.com>
2026-03-27 18:36:27 +0000

    Changed scale swizzling condition in test for gfx1250

--- op_tests/triton_tests/moe/test_moe_gemm_a4w4.py
+++ op_tests/triton_tests/moe/test_moe_gemm_a4w4.py
@@ -232,7 +232,11 @@ def test_op(
     device="cuda",
 ):
     if hbm_swizzling:
-        if n % 32 != 0 or k % (32 * 8) != 0:
+        if get_arch() == "gfx950" and m % 32 != 0 or k % (32 * 8) != 0:
+            pytest.skip(
+                f"Shape {m}x{n}x{k} is not supported for scale swizzling on AMD GPU"
+            )
+        elif get_arch() == "gfx1250" and k % (32 * 4) != 0:
             pytest.skip(
                 f"Shape {m}x{n}x{k} is not supported for scale swizzling on AMD GPU"
             )

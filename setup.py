0abd55a4740ed0b2904444d49565798a7befb48f
xudoyuan <xudoyuan@amd.com>
2026-05-08 06:42:18 +0000

    [FLYDSL]: black setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ OPT_COMPILER_CONFIG = os.path.join(this_dir, "aiter", "jit", "optCompilerConfig.
 PACKAGE_NAME = "amd-aiter"
 
 FLYDSL_VERSION = "flydsl==0.1.6.dev20260508"
-FLYDSL_FIND_LINKS = "https://rocm.frameworks-devreleases.amd.com/whl/gfx942-gfx950/flydsl/"
+FLYDSL_FIND_LINKS = (
+    "https://rocm.frameworks-devreleases.amd.com/whl/gfx942-gfx950/flydsl/"
+)
 
 BUILD_TARGET = os.environ.get("BUILD_TARGET", "auto")
 PREBUILD_KERNELS = int(os.environ.get("PREBUILD_KERNELS", 0))

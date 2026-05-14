e152ab89eb68b852273c6f673d39de235aafff3a
ShaoChunLee <Shao-Chun.Lee@amd.com>
2026-05-14 13:54:03 +0000

    add mhc post pre fusions

--- aiter/ops/triton/_triton_kernels/fusions/__init__.py
+++ aiter/ops/triton/_triton_kernels/fusions/__init__.py
@@ -6,6 +6,7 @@ from aiter.ops.triton._triton_kernels.fusions.mhc import (
     _mhc_fused_split_kernel,
     _mhc_reduce_apply_kernel,
     _mhc_post_kernel,
+    _mhc_post_pre_split_kernel,
 )
 
 __all__ = [
@@ -13,4 +14,5 @@ __all__ = [
     "_mhc_fused_split_kernel",
     "_mhc_reduce_apply_kernel",
     "_mhc_post_kernel",
+    "_mhc_post_pre_split_kernel",
 ]

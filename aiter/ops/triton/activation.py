5debdacefd2826e863521ce3fae6f6c723e3dac1
Tianxing Wu <chi0chu319@gmail.com>
2026-05-15 05:15:05 +0000

    move fp8_dtype = aiter.dtypes.fp8 after imports

--- aiter/ops/triton/activation.py
+++ aiter/ops/triton/activation.py
@@ -2,13 +2,13 @@ from typing import Literal, Optional
 import triton
 import torch
 import aiter
-
-fp8_dtype = aiter.dtypes.fp8
 from aiter.ops.triton.utils.logger import AiterTritonLogger
 from aiter.ops.triton._triton_kernels.activation import (
     _act_mul_and_dynamic_mxfp4_quant_kernel,
     _act_mul_and_dynamic_fp8_group_quant_kernel,
 )
+
+fp8_dtype = aiter.dtypes.fp8
 
 _LOGGER = AiterTritonLogger()
 

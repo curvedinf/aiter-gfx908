jin-amd/aiter

Support `int64` data type for group sizes
IMPORTANT: Maybe it's worth keeping this commit and PRing it to AITER!

aiter/ops/triton/utils/gmm_common.py
@@ -56,6 +56,55 @@ assert (
 DTYPE: torch.dtype = dtype_from_str(DTYPE_STR)
 
 
+# Supported integer data types for group sizes tensor.
+# ------------------------------------------------------------------------------
+
+# Supported data types, as strings.
+SUPPORTED_GROUP_SIZES_DTYPES_STR: set[str] = {"int32", "int64"}
+
+
+# Convert string data type to PyTorch data type.
+def group_sizes_dtype_from_str(dtype_str: str) -> torch.dtype:
+    dtype_str = dtype_str.strip().lower()
+    assert (
+        dtype_str in SUPPORTED_GROUP_SIZES_DTYPES_STR
+    ), "String data type isn't in set of supported string data types."
+    return {"int32": torch.int32, "int64": torch.int64}[dtype_str]
+
+
+# Supported data types, as PyTorch types.
+SUPPORTED_GROUP_SIZES_DTYPES: set[torch.dtype] = {
+    group_sizes_dtype_from_str(dtype_str)
+    for dtype_str in SUPPORTED_GROUP_SIZES_DTYPES_STR
+}
+
+
+# Convert PyTorch data type to string data type.
+def str_from_group_sizes_dtype(dtype: torch.dtype) -> str:
+    assert (
+        dtype in SUPPORTED_GROUP_SIZES_DTYPES
+    ), "PyTorch data type isn't in set of supported PyTorch data types."
+    return {torch.int32: "int32", torch.int64: "int64"}[dtype]
+
+
+# Default data type, as string.
+GROUP_SIZES_DTYPE_STR: str = "int64"
+assert (
+    GROUP_SIZES_DTYPE_STR in SUPPORTED_GROUP_SIZES_DTYPES_STR
+), "Default string data type isn't in set of supported string data types."
+
+
+# Default data type, as PyTorch type.
+GROUP_SIZES_DTYPE: torch.dtype = group_sizes_dtype_from_str(GROUP_SIZES_DTYPE_STR)
+
+
+def check_group_sizes_dtype(dtype: torch.dtype) -> None:
+    assert dtype in SUPPORTED_GROUP_SIZES_DTYPES, (
+        f"group_sizes data type must be one of {SUPPORTED_GROUP_SIZES_DTYPES}, "
+        f"got {dtype}."
+    )
+
+
 # Other defaults.
 # ------------------------------------------------------------------------------
 
@@ -90,7 +139,7 @@ def check_input_device_dtype(
     assert (
         lhs.dtype == rhs.dtype
     ), f"lhs and rhs types must match (lhs = {lhs.dtype}, rhs = {rhs.dtype})."
-    assert group_sizes.dtype == torch.int32, "group_sizes type must be int32."
+    check_group_sizes_dtype(group_sizes.dtype)
 
     if bias is not None:
         assert (
@@ -122,13 +171,15 @@ def gen_uniform_group_sizes(
     M: int,
     G: int,
     device: torch.device | str = DEVICE,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
 ) -> Tensor:
     assert M >= 0, f"Number of tokens M must be non-negative (it's {M})."
     assert G > 0, f"Number of experts G must be positive (it's {G})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     base = M // G
     remainder = M % G
-    group_sizes = torch.full((G,), base, dtype=torch.int32, device=device)
+    group_sizes = torch.full((G,), base, dtype=group_sizes_dtype, device=device)
     if remainder > 0:
         group_sizes[:remainder] += 1
 
@@ -139,7 +190,9 @@ def gen_uniform_group_sizes(
     assert (
         torch.sum(group_sizes).item() == M
     ), f"Group sizes don't add up to total tokens {M}."
-    assert group_sizes.dtype == torch.int32, "Group sizes must be int32."
+    assert (
+        group_sizes.dtype == group_sizes_dtype
+    ), f"Group sizes must be {group_sizes_dtype} (it's {group_sizes.dtype})."
 
     return group_sizes
 
@@ -151,6 +204,7 @@ def gen_group_sizes(
     rng_seed: int | None = RNG_SEED,
     unused_tokens_prob: float = UNUSED_TOKENS_PROB,
     unused_experts_prob: float = UNUSED_EXPERTS_PROB,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
 ) -> Tensor:
     assert M >= 0, f"Number of tokens M must be non-negative (it's {M})."
     assert G > 0, f"Number of experts G must be positive (it's {G})."
@@ -160,6 +214,7 @@ def gen_group_sizes(
     assert (
         0 <= unused_experts_prob <= 1
     ), f"Probability of unused experts must be in [0, 1] interval (it's {unused_experts_prob})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     if rng_seed is not None:
         torch.manual_seed(rng_seed)
@@ -224,7 +279,7 @@ def gen_group_sizes(
             torch.randint(low=0, high=num_used_experts, size=(num_used_tokens,))
         ],
         minlength=G,
-    ).to(torch.int32)
+    ).to(group_sizes_dtype)
 
     assert (
         len(group_sizes) == G
@@ -233,7 +288,9 @@ def gen_group_sizes(
     assert (
         torch.sum(group_sizes).item() == num_used_tokens
     ), f"Group sizes don't add up to used tokens {num_used_tokens}."
-    assert group_sizes.dtype == torch.int32, "Group sizes must be int32."
+    assert (
+        group_sizes.dtype == group_sizes_dtype
+    ), f"Group sizes must be {group_sizes_dtype} (it's {group_sizes.dtype})."
 
     return group_sizes
 
@@ -247,10 +304,17 @@ def gen_multiple_group_sizes(
     unused_tokens_prob: float = UNUSED_TOKENS_PROB,
     unused_experts_prob: float = UNUSED_EXPERTS_PROB,
     group_sizes_0: Tensor | None = None,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
 ) -> list[Tensor]:
     assert (
         num_group_sizes > 0
     ), f"Number of group sizes to be generated must be positive, it's {num_group_sizes}."
+    check_group_sizes_dtype(group_sizes_dtype)
+    if group_sizes_0 is not None:
+        assert group_sizes_0.dtype == group_sizes_dtype, (
+            f"group_sizes_0 dtype ({group_sizes_0.dtype}) must match requested "
+            f"group_sizes_dtype ({group_sizes_dtype})."
+        )
     multiple_group_sizes = [
         gen_group_sizes(
             M,
@@ -259,6 +323,7 @@ def gen_multiple_group_sizes(
             rng_seed=rng_seed if g == 0 else None,
             unused_tokens_prob=unused_tokens_prob,
             unused_experts_prob=unused_experts_prob,
+            group_sizes_dtype=group_sizes_dtype,
         )
         for g in range(
             num_group_sizes if group_sizes_0 is None else num_group_sizes - 1
@@ -286,11 +351,13 @@ def gen_gmm_input(
     trans_rhs: bool = TRANS_RHS,
     rng_seed: int | None = RNG_SEED,
     unif_group_sizes: bool = False,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
 ) -> tuple[Tensor, Tensor, Tensor]:
     assert M > 0, f"Number of lhs rows M must be positive (M = {M})."
     assert K > 0, f"Number of lhs columns / rhs rows K must be positive (K = {K})."
     assert N > 0, f"Number of rhs columns N must be positive (N = {N})."
     assert G > 0, f"Number of groups G must be positive (G = {G})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     if rng_seed is not None:
         torch.manual_seed(rng_seed)
@@ -313,9 +380,13 @@ def gen_gmm_input(
     rhs = rhs.to(preferred_element_type)
 
     group_sizes = (
-        gen_uniform_group_sizes(M, G, device=device)
+        gen_uniform_group_sizes(
+            M, G, device=device, group_sizes_dtype=group_sizes_dtype
+        )
         if unif_group_sizes
-        else gen_group_sizes(M, G, device=device, rng_seed=None)
+        else gen_group_sizes(
+            M, G, device=device, rng_seed=None, group_sizes_dtype=group_sizes_dtype
+        )
     )
 
     return lhs, rhs, group_sizes
@@ -349,6 +420,7 @@ def gen_gmm_tensors(
     rng_seed: int | None = RNG_SEED,
     unif_group_sizes: bool = False,
     use_bias: bool = False,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
 ) -> tuple[Tensor, Tensor, list[Tensor], Tensor, Tensor | None]:
     lhs, rhs, group_sizes_0 = gen_gmm_input(
         M,
@@ -360,9 +432,16 @@ def gen_gmm_tensors(
         trans_rhs=trans_rhs,
         rng_seed=rng_seed,
         unif_group_sizes=unif_group_sizes,
+        group_sizes_dtype=group_sizes_dtype,
     )
     multiple_group_sizes = gen_multiple_group_sizes(
-        num_group_sizes, M, G, device=device, rng_seed=None, group_sizes_0=group_sizes_0
+        num_group_sizes,
+        M,
+        G,
+        device=device,
+        rng_seed=None,
+        group_sizes_0=group_sizes_0,
+        group_sizes_dtype=group_sizes_dtype,
     )
     out = gen_gmm_output(M, N, device=device, preferred_element_type=output_type)
     bias = None
@@ -526,11 +605,13 @@ def gen_tgmm_input(
     trans_lhs: bool = TRANS_LHS,
     rng_seed: int | None = RNG_SEED,
     unif_group_sizes: bool = False,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
 ) -> tuple[Tensor, Tensor, Tensor]:
     assert K > 0, f"Number of lhs rows K must be positive (M = {K})."
     assert M > 0, f"Number of lhs columns / rhs rows M must be positive (K = {M})."
     assert N > 0, f"Number of rhs columns N must be positive (N = {N})."
     assert G > 0, f"Number of groups G must be positive (G = {G})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     if rng_seed is not None:
         torch.manual_seed(rng_seed)
@@ -545,9 +626,13 @@ def gen_tgmm_input(
     rhs = rhs.to(preferred_element_type)
 
     group_sizes = (
-        gen_uniform_group_sizes(M, G, device=device)
+        gen_uniform_group_sizes(
+            M, G, device=device, group_sizes_dtype=group_sizes_dtype
+        )
         if unif_group_sizes
-        else gen_group_sizes(M, G, device=device, rng_seed=None)
+        else gen_group_sizes(
+            M, G, device=device, rng_seed=None, group_sizes_dtype=group_sizes_dtype
+        )
     )
 
     return lhs, rhs, group_sizes
@@ -600,6 +685,7 @@ def gen_tgmm_tensors(
     rng_seed: int | None = RNG_SEED,
     unif_group_sizes: bool = False,
     use_bias: bool = False,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
 ) -> tuple[Tensor, Tensor, list[Tensor], Tensor, Tensor | None]:
     lhs, rhs, group_sizes_0 = gen_tgmm_input(
         M,
@@ -611,9 +697,16 @@ def gen_tgmm_tensors(
         trans_lhs=trans_lhs,
         rng_seed=rng_seed,
         unif_group_sizes=unif_group_sizes,
+        group_sizes_dtype=group_sizes_dtype,
     )
     multiple_group_sizes = gen_multiple_group_sizes(
-        num_group_sizes, M, G, device=device, rng_seed=None, group_sizes_0=group_sizes_0
+        num_group_sizes,
+        M,
+        G,
+        device=device,
+        rng_seed=None,
+        group_sizes_0=group_sizes_0,
+        group_sizes_dtype=group_sizes_dtype,
     )
     out = gen_tgmm_output(K, N, G, device=device, preferred_element_type=output_type)
     if use_bias:

curvedinf/aiter-gfx908

aiter/ops/triton/utils/gmm_common.py
@@ -56,6 +56,55 @@ assert (
 DTYPE: torch.dtype = dtype_from_str(DTYPE_STR)
 
 
+# Supported integer data types for group sizes tensor.
+# ------------------------------------------------------------------------------
+
+# Supported group sizes data types, as strings.
+SUPPORTED_GROUP_SIZES_DTYPES_STR: set[str] = {"int32", "int64"}
+
+
+# Convert string data type to PyTorch data type.
+def group_sizes_dtype_from_str(dtype_str: str) -> torch.dtype:
+    dtype_str = dtype_str.strip().lower()
+    assert (
+        dtype_str in SUPPORTED_GROUP_SIZES_DTYPES_STR
+    ), "String data type isn't in set of supported string data types."
+    return {"int32": torch.int32, "int64": torch.int64}[dtype_str]
+
+
+# Supported data types, as PyTorch types.
+SUPPORTED_GROUP_SIZES_DTYPES: set[torch.dtype] = {
+    group_sizes_dtype_from_str(dtype_str)
+    for dtype_str in SUPPORTED_GROUP_SIZES_DTYPES_STR
+}
+
+
+# Convert PyTorch data type to string data type.
+def str_from_group_sizes_dtype(dtype: torch.dtype) -> str:
+    assert (
+        dtype in SUPPORTED_GROUP_SIZES_DTYPES
+    ), "PyTorch data type isn't in set of supported PyTorch data types."
+    return {torch.int32: "int32", torch.int64: "int64"}[dtype]
+
+
+# Default data type, as string.
+GROUP_SIZES_DTYPE_STR: str = "int32"
+assert (
+    GROUP_SIZES_DTYPE_STR in SUPPORTED_GROUP_SIZES_DTYPES_STR
+), "Default string data type isn't in set of supported string data types."
+
+
+# Default data type, as PyTorch type.
+GROUP_SIZES_DTYPE: torch.dtype = group_sizes_dtype_from_str(GROUP_SIZES_DTYPE_STR)
+
+
+def check_group_sizes_dtype(dtype: torch.dtype) -> None:
+    assert dtype in SUPPORTED_GROUP_SIZES_DTYPES, (
+        f"group_sizes data type must be one of {SUPPORTED_GROUP_SIZES_DTYPES}, "
+        f"got {dtype}."
+    )
+
+
 # Other defaults.
 # ------------------------------------------------------------------------------
 
@@ -90,7 +139,7 @@ def check_input_device_dtype(
     assert (
         lhs.dtype == rhs.dtype
     ), f"lhs and rhs types must match (lhs = {lhs.dtype}, rhs = {rhs.dtype})."
-    assert group_sizes.dtype == torch.int32, "group_sizes type must be int32."
+    check_group_sizes_dtype(group_sizes.dtype)
 
     if bias is not None:
         assert (
@@ -121,14 +170,16 @@ UNUSED_EXPERTS_PROB: float = 0.1
 def gen_uniform_group_sizes(
     M: int,
     G: int,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
     device: torch.device | str = DEVICE,
 ) -> Tensor:
     assert M >= 0, f"Number of tokens M must be non-negative (it's {M})."
     assert G > 0, f"Number of experts G must be positive (it's {G})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     base = M // G
     remainder = M % G
-    group_sizes = torch.full((G,), base, dtype=torch.int32, device=device)
+    group_sizes = torch.full((G,), base, dtype=group_sizes_dtype, device=device)
     if remainder > 0:
         group_sizes[:remainder] += 1
 
@@ -139,7 +190,9 @@ def gen_uniform_group_sizes(
     assert (
         torch.sum(group_sizes).item() == M
     ), f"Group sizes don't add up to total tokens {M}."
-    assert group_sizes.dtype == torch.int32, "Group sizes must be int32."
+    assert (
+        group_sizes.dtype == group_sizes_dtype
+    ), f"Group sizes must be {group_sizes_dtype} (it's {group_sizes.dtype})."
 
     return group_sizes
 
@@ -147,6 +200,7 @@ def gen_uniform_group_sizes(
 def gen_group_sizes(
     M: int,
     G: int,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
     device: torch.device | str = DEVICE,
     rng_seed: int | None = RNG_SEED,
     unused_tokens_prob: float = UNUSED_TOKENS_PROB,
@@ -160,6 +214,7 @@ def gen_group_sizes(
     assert (
         0 <= unused_experts_prob <= 1
     ), f"Probability of unused experts must be in [0, 1] interval (it's {unused_experts_prob})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     if rng_seed is not None:
         torch.manual_seed(rng_seed)
@@ -224,7 +279,7 @@ def gen_group_sizes(
             torch.randint(low=0, high=num_used_experts, size=(num_used_tokens,))
         ],
         minlength=G,
-    ).to(torch.int32)
+    ).to(group_sizes_dtype)
 
     assert (
         len(group_sizes) == G
@@ -233,7 +288,9 @@ def gen_group_sizes(
     assert (
         torch.sum(group_sizes).item() == num_used_tokens
     ), f"Group sizes don't add up to used tokens {num_used_tokens}."
-    assert group_sizes.dtype == torch.int32, "Group sizes must be int32."
+    assert (
+        group_sizes.dtype == group_sizes_dtype
+    ), f"Group sizes must be {group_sizes_dtype} (it's {group_sizes.dtype})."
 
     return group_sizes
 
@@ -242,6 +299,7 @@ def gen_multiple_group_sizes(
     num_group_sizes: int,
     M: int,
     G: int,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
     device: torch.device | str = DEVICE,
     rng_seed: int | None = RNG_SEED,
     unused_tokens_prob: float = UNUSED_TOKENS_PROB,
@@ -251,10 +309,17 @@ def gen_multiple_group_sizes(
     assert (
         num_group_sizes > 0
     ), f"Number of group sizes to be generated must be positive, it's {num_group_sizes}."
+    check_group_sizes_dtype(group_sizes_dtype)
+    if group_sizes_0 is not None:
+        assert group_sizes_0.dtype == group_sizes_dtype, (
+            f"group_sizes_0 dtype ({group_sizes_0.dtype}) must match requested "
+            f"group_sizes_dtype ({group_sizes_dtype})."
+        )
     multiple_group_sizes = [
         gen_group_sizes(
             M,
             G,
+            group_sizes_dtype=group_sizes_dtype,
             device=device,
             rng_seed=rng_seed if g == 0 else None,
             unused_tokens_prob=unused_tokens_prob,
@@ -281,8 +346,9 @@ def gen_gmm_input(
     K: int,
     N: int,
     G: int,
-    device: torch.device | str = DEVICE,
     preferred_element_type: torch.dtype = DTYPE,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
+    device: torch.device | str = DEVICE,
     trans_rhs: bool = TRANS_RHS,
     alt_trans: bool = False,
     rng_seed: int | None = RNG_SEED,
@@ -292,6 +358,7 @@ def gen_gmm_input(
     assert K > 0, f"Number of lhs columns / rhs rows K must be positive (K = {K})."
     assert N > 0, f"Number of rhs columns N must be positive (N = {N})."
     assert G > 0, f"Number of groups G must be positive (G = {G})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     if rng_seed is not None:
         torch.manual_seed(rng_seed)
@@ -320,9 +387,20 @@ def gen_gmm_input(
     rhs = rhs.to(preferred_element_type)
 
     group_sizes = (
-        gen_uniform_group_sizes(M, G, device=device)
+        gen_uniform_group_sizes(
+            M,
+            G,
+            group_sizes_dtype=group_sizes_dtype,
+            device=device,
+        )
         if unif_group_sizes
-        else gen_group_sizes(M, G, device=device, rng_seed=None)
+        else gen_group_sizes(
+            M,
+            G,
+            group_sizes_dtype=group_sizes_dtype,
+            device=device,
+            rng_seed=None,
+        )
     )
 
     return lhs, rhs, group_sizes
@@ -331,8 +409,8 @@ def gen_gmm_input(
 def gen_gmm_output(
     M: int,
     N: int,
-    device: torch.device | str = DEVICE,
     preferred_element_type: torch.dtype = DTYPE,
+    device: torch.device | str = DEVICE,
 ) -> Tensor:
     assert M > 0, f"Number of out rows M must be positive (M = {M})."
     assert N > 0, f"Number of out columns N must be positive (N = {N})."
@@ -348,9 +426,10 @@ def gen_gmm_tensors(
     N: int,
     G: int,
     num_group_sizes: int,
-    device: torch.device | str = DEVICE,
     input_type: torch.dtype = DTYPE,
     output_type: torch.dtype = DTYPE,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
+    device: torch.device | str = DEVICE,
     trans_lhs: bool = False,
     trans_rhs: bool = TRANS_RHS,
     alt_trans: bool = False,
@@ -363,17 +442,24 @@ def gen_gmm_tensors(
         K,
         N,
         G,
-        device=device,
         preferred_element_type=input_type,
+        group_sizes_dtype=group_sizes_dtype,
+        device=device,
         trans_rhs=trans_rhs,
         alt_trans=alt_trans,
         rng_seed=rng_seed,
         unif_group_sizes=unif_group_sizes,
     )
     multiple_group_sizes = gen_multiple_group_sizes(
-        num_group_sizes, M, G, device=device, rng_seed=None, group_sizes_0=group_sizes_0
+        num_group_sizes,
+        M,
+        G,
+        group_sizes_dtype=group_sizes_dtype,
+        device=device,
+        rng_seed=None,
+        group_sizes_0=group_sizes_0,
     )
-    out = gen_gmm_output(M, N, device=device, preferred_element_type=output_type)
+    out = gen_gmm_output(M, N, preferred_element_type=output_type, device=device)
     bias = None
     if use_bias:
         torch.manual_seed(rng_seed + 1000)  # Different seed for bias
@@ -547,8 +633,9 @@ def gen_tgmm_input(
     K: int,
     N: int,
     G: int,
-    device: torch.device | str = DEVICE,
     preferred_element_type: torch.dtype = DTYPE,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
+    device: torch.device | str = DEVICE,
     trans_lhs: bool = TRANS_LHS,
     alt_trans: bool = False,
     rng_seed: int | None = RNG_SEED,
@@ -558,6 +645,7 @@ def gen_tgmm_input(
     assert M > 0, f"Number of lhs columns / rhs rows M must be positive (K = {M})."
     assert N > 0, f"Number of rhs columns N must be positive (N = {N})."
     assert G > 0, f"Number of groups G must be positive (G = {G})."
+    check_group_sizes_dtype(group_sizes_dtype)
 
     if rng_seed is not None:
         torch.manual_seed(rng_seed)
@@ -583,9 +671,20 @@ def gen_tgmm_input(
     rhs = rhs.to(preferred_element_type)
 
     group_sizes = (
-        gen_uniform_group_sizes(M, G, device=device)
+        gen_uniform_group_sizes(
+            M,
+            G,
+            group_sizes_dtype=group_sizes_dtype,
+            device=device,
+        )
         if unif_group_sizes
-        else gen_group_sizes(M, G, device=device, rng_seed=None)
+        else gen_group_sizes(
+            M,
+            G,
+            group_sizes_dtype=group_sizes_dtype,
+            device=device,
+            rng_seed=None,
+        )
     )
 
     return lhs, rhs, group_sizes
@@ -595,8 +694,8 @@ def gen_tgmm_output(
     K: int,
     N: int,
     G: int,
-    device: torch.device | str = DEVICE,
     preferred_element_type: torch.dtype = DTYPE,
+    device: torch.device | str = DEVICE,
 ) -> Tensor:
     assert K > 0, f"Number of out rows K must be positive (K = {K})."
     assert N > 0, f"Number of out columns N must be positive (N = {N})."
@@ -630,9 +729,10 @@ def gen_tgmm_tensors(
     N: int,
     G: int,
     num_group_sizes: int,
-    device: torch.device | str = DEVICE,
     input_type: torch.dtype = DTYPE,
     output_type: torch.dtype = DTYPE,
+    group_sizes_dtype: torch.dtype = GROUP_SIZES_DTYPE,
+    device: torch.device | str = DEVICE,
     trans_lhs: bool = TRANS_LHS,
     trans_rhs: bool = False,
     alt_trans: bool = False,
@@ -645,17 +745,24 @@ def gen_tgmm_tensors(
         K,
         N,
         G,
-        device=device,
         preferred_element_type=input_type,
+        group_sizes_dtype=group_sizes_dtype,
+        device=device,
         trans_lhs=trans_lhs,
         alt_trans=alt_trans,
         rng_seed=rng_seed,
         unif_group_sizes=unif_group_sizes,
     )
     multiple_group_sizes = gen_multiple_group_sizes(
-        num_group_sizes, M, G, device=device, rng_seed=None, group_sizes_0=group_sizes_0
+        num_group_sizes,
+        M,
+        G,
+        group_sizes_dtype=group_sizes_dtype,
+        device=device,
+        rng_seed=None,
+        group_sizes_0=group_sizes_0,
     )
-    out = gen_tgmm_output(K, N, G, device=device, preferred_element_type=output_type)
+    out = gen_tgmm_output(K, N, G, preferred_element_type=output_type, device=device)
     if use_bias:
         bias_grad = gen_tgmm_bias_grad(K, G, device=device, with_bias_grad=True)
     else:
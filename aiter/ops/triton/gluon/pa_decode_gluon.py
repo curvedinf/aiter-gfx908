093a9316cd9122133c2b4ebd0c3ba3dfd9efbecc
jpvillam <jpvillam@amd.com>
2026-06-04 19:23:09 +0000

    hand changes from the docker image

--- aiter/ops/triton/gluon/pa_decode_gluon.py
+++ aiter/ops/triton/gluon/pa_decode_gluon.py
@@ -90,9 +90,10 @@ def _fused_max_combine(a1, a2, b1, b2):
 @lru_cache(maxsize=1)
 def get_cdna_version():
     """Get CDNA version lazily to avoid CUDA initialization during import."""
-    if arch_info.get_arch() in ["gfx950"]:
+    arch = arch_info.get_arch()
+    if arch in ["gfx950", "gfx1250"]:
         return 4
-    elif arch_info.get_arch() in ["gfx942"]:
+    elif arch in ["gfx942"]:
         return 3
     else:
         return -1
